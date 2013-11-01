5eb96c5719f93c4fcee2be3fec7a9449396162e6
Kamal Gill <kamal@kamalgill.net>
2013-10-31 17:45:27 -0700

    Enable client-side filtering of instances on instance landing page.

--- koala/static/js/pages/instances.js
+++ koala/static/js/pages/instances.js
@@ -18,5 +18,25 @@ angular.module('InstancesLandingPage', [])
                 $scope.instances = oData ? oData.results : [];
             });
         };
+        $scope.searchFilterInstances = function() {
+            // Filter instances client side based on search criteria.
+            // Leverages Array.prototype.filter and Object.keys, EcmaScript 5 methods supported in recent browsers
+            // TODO: Need to reset the instances array prior to performing subsequent filtering
+            var filterText = $scope.searchFilter;
+            var filteredInstances = $scope.instances.filter(function(item) {
+                var keys = Object.keys(item);
+                for (var i=0; i < keys.length; i++) {
+                    var itemProp = item[keys[i]];
+                    if (typeof itemProp === "string" && itemProp.indexOf(filterText) !== -1) {
+                        return item;
+                    }
+                }
+            });
+            if (filterText) {
+                $scope.instances = filteredInstances;
+            } else {
+                $scope.instances = $scope.getInstances();
+            }
+        };
     })
 ;
