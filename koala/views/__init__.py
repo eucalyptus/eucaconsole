31c4ce58255a84e2764d51c42b62d4c0db5153ff
Kamal Gill <kamal@kamalgill.net>
2013-11-04 20:12:26 -0800

    Add table view for landing pages and enable table/grid view switcher.

---
+++ koala/views/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core views
+
+"""
+
+
+class LandingPageView(object):
+    def __init__(self, request):
+        self.request = request
+        self.display_type = self.request.params.get('display', 'gridview')
+        self.filter_fields = []
+        self.filter_keys = []
--- koala/views/instances.py
+++ koala/views/instances.py
@@ -6,23 +6,23 @@ from pyramid.view import view_config
 
 from ..models import LandingPageFilter
 from ..models.instances import Instance
+from ..views import LandingPageView
 
 
-class InstancesView(object):
+class InstancesView(LandingPageView):
     def __init__(self, request):
-        self.request = request
+        super(InstancesView, self).__init__(request)
         self.instances = Instance.fakeall()
 
     @view_config(route_name='instances', renderer='../templates/instances/instances.pt', permission='view')
     def instances_landing(self):
-        # We could build the status choices list from Invoice.STATUS_CHOICES, but let's restrict based on the instances
         json_items_endpoint = self.request.route_url('instances_json')
         status_choices = sorted(set(instance.get('status') for instance in self.instances))
         root_device_choices = sorted(set(instance.get('root_device') for instance in self.instances))
         instance_type_choices = sorted(set(instance.get('instance_type') for instance in self.instances))
         security_group_choices = sorted(set(instance.get('security_group') for instance in self.instances))
         avail_zone_choices = sorted(set(instance.get('availability_zone') for instance in self.instances))
-        filter_fields = [
+        self.filter_fields = [
             LandingPageFilter(key='status', name='Status', choices=status_choices),
             LandingPageFilter(key='root_device', name='Root device', choices=root_device_choices),
             LandingPageFilter(key='instance_type', name='Instance type', choices=instance_type_choices),
@@ -30,11 +30,13 @@ class InstancesView(object):
             LandingPageFilter(key='availability_zone', name='Availability zone', choices=avail_zone_choices),
             LandingPageFilter(key='tags', name='Tags'),
         ]
-        filter_keys = [field.key for field in filter_fields]
+        more_filter_keys = ['instance_id']
+        self.filter_keys = [field.key for field in self.filter_fields] + more_filter_keys
 
         return dict(
-            filter_fields=filter_fields,
-            filter_keys=filter_keys,
+            display_type=self.display_type,
+            filter_fields=self.filter_fields,
+            filter_keys=self.filter_keys,
             json_items_endpoint=json_items_endpoint,
         )
 
