46a0a51aa711ffa4c33ca0fe2fe4abf1a44c91da
Kamal Gill <kamal@kamalgill.net>
2013-11-14 12:29:20 -0800

    Configure connection objects for AutoScaling and CloudWatch services.  Pull in scaling groups from Euca/AWS on landing page.  GUI-51

--- koala/views/scalinggroups.py
+++ koala/views/scalinggroups.py
@@ -6,31 +6,34 @@ Pyramid views for Eucalyptus and AWS scaling groups
 from pyramid.view import view_config
 
 from ..models import LandingPageFilter
-from ..models.scalinggroups import AutoScalingGroup
 from ..views import LandingPageView
 
 
 class ScalingGroupsView(LandingPageView):
     def __init__(self, request):
         super(ScalingGroupsView, self).__init__(request)
-        self.items = AutoScalingGroup.fakeall()
+        self.items = self.get_items()
         self.initial_sort_key = 'name'
         self.prefix = '/scalinggroups'
+
+    def get_items(self):
+        conn = self.get_connection(conn_type='autoscale')
+        return conn.get_all_groups()
 
     @view_config(route_name='scalinggroups', renderer='../templates/scalinggroups/scalinggroups.pt')
     def scalinggroups_landing(self):
         json_items_endpoint = self.request.route_url('scalinggroups_json')
         # Filter fields are passed to 'properties_filter_form' template macro to display filters at left
-        instance_health_choices = ['All healthy', 'Unhealthy']
+        launch_config_choices = sorted(set(item.launch_config for item in self.items))
         self.filter_fields = [
-            LandingPageFilter(key='instance_health', name='Instance Health', choices=instance_health_choices),
+            LandingPageFilter(key='launch_config', name='Launch Configuration', choices=launch_config_choices),
         ]
-        more_filter_keys = ['name', 'launch_config']
+        more_filter_keys = ['availability_zones', 'name', 'placement_group']
         self.filter_keys = [field.key for field in self.filter_fields] + more_filter_keys
         # sort_keys are passed to sorting drop-down
         self.sort_keys = [
             dict(key='name', name='Name'),
-            dict(key='instance_health', name='Instance Health'),
+            dict(key='launch_config', name='Launch configuration'),
         ]
 
         return dict(
@@ -45,5 +48,17 @@ class ScalingGroupsView(LandingPageView):
 
     @view_config(route_name='scalinggroups_json', renderer='json', request_method='GET')
     def scalinggroups_json(self):
-        return dict(results=self.items)
+        scalinggroups = []
+        for group in self.items:
+            scalinggroups.append(dict(
+                availability_zones=', '.join(group.availability_zones),
+                desired_capacity=group.desired_capacity,
+                launch_config=group.launch_config,
+                max_size=group.max_size,
+                min_size=group.min_size,
+                name=group.name,
+                placement_group=group.placement_group,
+                termination_policies=', '.join(group.termination_policies),
+            ))
+        return dict(results=scalinggroups)
 
