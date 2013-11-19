d81d4eca9d9ad91aa0757c616f86935bfb98f197
Kamal Gill <kamal@kamalgill.net>
2013-11-19 13:56:04 -0800

    Mark a few more translation strings.

--- koala/forms/login.py
+++ koala/forms/login.py
@@ -13,15 +13,15 @@ from . import BaseSecureForm
 
 class EucaLoginForm(BaseSecureForm):
     account = wtforms.TextField(
-        'Account Name', validators=[validators.Required(message=_(u'Account name is required'))])
+        _(u'Account Name'), validators=[validators.Required(message=_(u'Account name is required'))])
     username = wtforms.TextField(
-        'Username', validators=[validators.Required(message=_(u'Username is required'))])
+        _(u'Username'), validators=[validators.Required(message=_(u'Username is required'))])
     password = wtforms.PasswordField(
-        'Password', validators=[validators.Required(message=_(u'Password is required'))])
+        _(u'Password'), validators=[validators.Required(message=_(u'Password is required'))])
 
 
 class AWSLoginForm(BaseSecureForm):
     access_key = wtforms.TextField(
-        'Access key ID', validators=[validators.Required(message=_(u'Access key is required'))])
+        _(u'Access key ID'), validators=[validators.Required(message=_(u'Access key is required'))])
     secret_key = wtforms.PasswordField(
-        'Secret key', validators=[validators.Required(message=_(u'Secret key is required'))])
+        _(u'Secret key'), validators=[validators.Required(message=_(u'Secret key is required'))])
