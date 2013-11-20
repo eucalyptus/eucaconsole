5f32a61446312e73ccff25cdbcc0c12496052eb2
Kamal Gill <kamal@kamalgill.net>
2013-11-20 09:21:54 -0800

    Display a “required” indicator and add the required=“required” HTML attribute if a form field is marked with a required validator, keeping things DRY.  GUI-158

--- koala/views/panels.py
+++ koala/views/panels.py
@@ -9,6 +9,9 @@ from pyramid_layout.panel import panel_config
 @panel_config('form_field', renderer='../templates/panels/form_field_row.pt')
 def form_field_row(context, request, field=None, html_attrs=None):
     html_attrs = html_attrs or {}
+    # Add required="required" attributed to form field if any "required" validators are detected
+    if field.flags.required and html_attrs.get('required') is None:
+        html_attrs['required'] = 'required'
     error_msg = getattr(field, 'error_msg', None)
     return dict(field=field, error_msg=error_msg, html_attrs=html_attrs)
 
