304b07f8f8f8ddd3c8dbdcd1b00a36c5a377c71b
dkavanagh <dkavanagh@gmail.com>
2013-12-06 14:39:56 -0500

    working change password screen. Might want to tweak validation, but it works pretty well

--- koala/forms/login.py
+++ koala/forms/login.py
@@ -4,7 +4,7 @@ Eucalyptus and AWS login forms
 
 """
 import wtforms
-from wtforms import validators
+from wtforms import validators, widgets
 
 from pyramid.i18n import TranslationString as _
 
@@ -25,3 +25,23 @@ class AWSLoginForm(BaseSecureForm):
         _(u'Access key ID'), validators=[validators.Required(message=_(u'Access key is required'))])
     secret_key = wtforms.PasswordField(
         _(u'Secret key'), validators=[validators.Required(message=_(u'Secret key is required'))])
+
+class EucaChangePasswordForm(BaseSecureForm):
+    password = wtforms.PasswordField(
+        _(u'Password'), validators=[validators.Required(message=_(u'Password is required'))],
+        widget=widgets.PasswordInput())
+    new_password = wtforms.PasswordField(
+        _(u'New Password'),
+        validators=[
+            validators.Required(message=_(u'New Password is required')),
+            validators.Length(min=6, message=_(u'Password must be more than 6 characters'))
+        ],
+        widget=widgets.PasswordInput())
+    new_password2 = wtforms.PasswordField(
+        _(u'New Password'),
+        validators=[
+            validators.Required(message=_(u'New Password is required')),
+            validators.Length(min=6, message=_(u'Password must be more than 6 characters'))
+        ],
+        widget=widgets.PasswordInput())
+        
