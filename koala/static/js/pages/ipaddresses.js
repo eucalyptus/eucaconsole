4e2d2630a6e3aed0b3c0847b4b4a3a55c7fcc681
Kamal Gill <kamal@kamalgill.net>
2013-12-10 20:11:17 -0800

    Right-align button dropdown menus on landing pages and add drop shadow for visibility.  GUI-55

---
+++ koala/static/js/pages/ipaddresses.js
@@ -0,0 +1,12 @@
+/**
+ * @fileOverview Elastic IPs landing page JS
+ * @requires AngularJS, jQuery
+ *
+ */
+
+// Pull in common landing page module
+angular.module('ElasticIPsPage', ['LandingPage'])
+    .controller('ElasticIPsCtrl', function ($scope, $http) {
+        // pass
+    });
+;
