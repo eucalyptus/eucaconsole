290c08cefa8438dacb8f6d6b057eda415909ee86
Kamal Gill <kamal@kamalgill.net>
2013-12-16 22:29:26 -0800

    Add ‘Next step’ button to proceed to next wizard step, triggering validation feedback if there are any errors.  GUI-195

--- koala/static/js/pages/instance_launch.js
+++ koala/static/js/pages/instance_launch.js
@@ -6,8 +6,18 @@
 
 // Launch Instance page includes the tag editor, so pull in that module as well.
 angular.module('LaunchInstance', ['TagEditor'])
-    .controller('LaunchInstanceCtrl', function ($scope, $http, $timeout) {
-        // TODO: Implement
+    .controller('LaunchInstanceCtrl', function ($scope) {
+        $scope.form = $('#launch-instance-form');
+        $scope.visitStep = function (step, $event) {
+            $scope.form.trigger('validate');
+            var invalidFields = $scope.form.find('[data-invalid]');
+            if (invalidFields.length) {
+                invalidFields.focus();
+                $event.preventDefault();
+                return false;
+            }
+            $('#tabStep' + step).click();
+        };
     })
 ;
 
