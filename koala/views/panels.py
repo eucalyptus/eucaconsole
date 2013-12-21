98a775554f4540c3fc9a1197e54928e359a9ec46
Kamal Gill <kamal@kamalgill.net>
2013-12-20 18:09:27 -0800

    Display scroll bar for image picker.  GUI-195

--- koala/views/panels.py
+++ koala/views/panels.py
@@ -123,9 +123,9 @@ def bdmapping_editor(context, request, image=None):
 
 
 @panel_config('image_picker', renderer='../templates/panels/image_picker.pt')
-def image_picker(context, request, image=None, images_json_endpoint=None):
+def image_picker(context, request, image=None, images_json_endpoint=None, maxheight='800px'):
     """ Reusable Image picker widget (e.g. for Launch Instance page, step 1).
         Usage example (in Chameleon template): ${panel('image_picker')}
     """
-    return dict(image=image, images_json_endpoint=images_json_endpoint)
+    return dict(image=image, images_json_endpoint=images_json_endpoint, maxheight=maxheight)
 
