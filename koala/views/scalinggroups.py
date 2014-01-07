21b3d6841a3338175265207851df301b868d530a
Kamal Gill <kamal@kamalgill.net>
2014-01-07 14:06:31 -0800

    Scaling Groups landing page cleanup.  GUI-51

--- koala/views/scalinggroups.py
+++ koala/views/scalinggroups.py
@@ -16,30 +16,20 @@ from ..views import LandingPageView, BaseView
 class ScalingGroupsView(LandingPageView):
     def __init__(self, request):
         super(ScalingGroupsView, self).__init__(request)
-        self.items = self.get_items()
         self.initial_sort_key = 'name'
         self.prefix = '/scalinggroups'
-
-    def get_items(self):
-        conn = self.get_connection(conn_type='autoscale')
-        return conn.get_all_groups() if conn else []
+        self.display_type = self.request.params.get('display', 'tableview')  # Set tableview as default
 
     @view_config(route_name='scalinggroups', renderer='../templates/scalinggroups/scalinggroups.pt')
     def scalinggroups_landing(self):
         json_items_endpoint = self.request.route_url('scalinggroups_json')
-        # Filter fields are passed to 'properties_filter_form' template macro to display filters at left
-        launch_config_choices = sorted(set(item.launch_config_name for item in self.items))
-        self.filter_fields = [
-            LandingPageFilter(key='launch_config', name=_(u'Launch Configuration'), choices=launch_config_choices),
-        ]
-        more_filter_keys = ['availability_zones', 'name', 'placement_group']
-        self.filter_keys = [field.key for field in self.filter_fields] + more_filter_keys
+        self.filter_keys = ['availability_zones', 'launch_config', 'name', 'placement_group']
         # sort_keys are passed to sorting drop-down
         self.sort_keys = [
             dict(key='name', name=_(u'Name')),
             dict(key='launch_config', name=_(u'Launch configuration')),
+            dict(key='availability_zones', name=_(u'Availability zones')),
         ]
-
         return dict(
             display_type=self.display_type,
             filter_fields=self.filter_fields,
@@ -50,10 +40,12 @@ class ScalingGroupsView(LandingPageView):
             json_items_endpoint=json_items_endpoint,
         )
 
+
+class ScalingGroupsJsonView(BaseView):
     @view_config(route_name='scalinggroups_json', renderer='json', request_method='GET')
     def scalinggroups_json(self):
         scalinggroups = []
-        for group in self.items:
+        for group in self.get_items():
             scalinggroups.append(dict(
                 availability_zones=', '.join(group.availability_zones),
                 desired_capacity=group.desired_capacity,
@@ -65,6 +57,10 @@ class ScalingGroupsView(LandingPageView):
                 termination_policies=', '.join(group.termination_policies),
             ))
         return dict(results=scalinggroups)
+
+    def get_items(self):
+        conn = self.get_connection(conn_type='autoscale')
+        return conn.get_all_groups() if conn else []
 
 
 class ScalingGroupView(BaseView):
