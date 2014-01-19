6ea79e739f146c38efd026fd1b5fdcde230c3489
Kamal Gill <kamal@kamalgill.net>
2014-01-19 14:16:47 -0800

    Select dimension based on metric namespace in Create Alarm dialog.  GUI-215

--- koala/static/js/pages/create_alarm.js
+++ koala/static/js/pages/create_alarm.js
@@ -6,11 +6,9 @@
 angular.module('CreateAlarm', [])
     .controller('CreateAlarmCtrl', function ($scope) {
         $scope.alarmDialog = $('#create-alarm-modal');
-        $scope.metric = '';
-        $scope.namespace = '';
         $scope.updateDimensionChoices = function () {
             var selectedOptionLabel = $('#metric').find('option[value="' + $scope.metric + '"]').text();
             $scope.namespace = selectedOptionLabel.split(' - ')[0];
-        }
+        };
     })
 ;
