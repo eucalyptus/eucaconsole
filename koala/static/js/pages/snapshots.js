4a6bcb2245ed7115a7b36d1956e6154f3396aa39
Kyo Lee <kyolee310@gmail.com>
2014-01-22 21:18:43 -0800

    Added a watch function to ensure the dropdown box is closed after clicked

--- koala/static/js/pages/snapshots.js
+++ koala/static/js/pages/snapshots.js
@@ -24,6 +24,12 @@ angular.module('SnapshotsPage', ['CustomFilters'])
         $scope.itemsLoading = true;
         $scope.setInitialSort = function (sortKey) {
             $scope.sortBy = sortKey;
+            $scope.$watch('sortBy',  function () { 
+                if ($('#sorting-dropdown').hasClass('open')) { 
+                    $('#sorting-dropdown').removeClass('open'); 
+                    $('#sorting-dropdown').removeAttr('style'); 
+                } 
+            });
         };
         $scope.initController = function (sortKey, jsonItemsEndpoint) {
             $scope.jsonEndpoint = jsonItemsEndpoint;
