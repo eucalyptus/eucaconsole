d0f93ff22d28fa1111f5f4e45b8efc70e2009a1a
Kyo Lee <kyolee310@gmail.com>
2014-01-30 15:12:43 -0800

    Refactoring the image help panel. Empty content for now

--- koala/views/help.py
+++ koala/views/help.py
@@ -10,7 +10,31 @@ from pyramid_layout.panel import panel_config
 def help_ipaddresses(context, request, display):
     return dict(display=display)
 
+@panel_config('help_keypairs', renderer='../templates/help/help_keypairs.pt')
+def help_keypairs(context, request, display):
+    return dict(display=display)
 
+@panel_config('help_images', renderer='../templates/help/help_images.pt')
+def help_images(context, request, display):
+    return dict(display=display)
 
+@panel_config('help_instances', renderer='../templates/help/help_instances.pt')
+def help_instances(context, request, display):
+    return dict(display=display)
 
+@panel_config('help_volumes', renderer='../templates/help/help_volumes.pt')
+def help_volumes(context, request, display):
+    return dict(display=display)
+
+@panel_config('help_snapshots', renderer='../templates/help/help_snapshots.pt')
+def help_snapshots(context, request, display):
+    return dict(display=display)
+
+@panel_config('help_launchconfigs', renderer='../templates/help/help_launchconfigs.pt')
+def help_launchconfigs(context, request, display):
+    return dict(display=display)
+
+@panel_config('help_scalinggroups', renderer='../templates/help/help_scalinggroups.pt')
+def help_scalinggroups(context, request, display):
+    return dict(display=display)
 
