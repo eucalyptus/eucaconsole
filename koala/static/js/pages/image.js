7838c4023f9bbf5614fb6c9cfd93491718766fec
Kyo Lee <kyolee310@gmail.com>
2014-02-04 14:44:10 -0800

    Fixed the help panel mistakes. Turned out the JS files for image and security group detail pages were needed. For other pages, we need a dummy 'ng-app=""' attr

---
+++ koala/static/js/pages/image.js
@@ -0,0 +1,19 @@
+/**
+ *  * @fileOverview Image Detaile Page JS
+ *   * @requires AngularJS
+ *    *
+ *     */
+
+angular.module('ImagePage', ['TagEditor'])
+    .controller('ImagePageCtrl', function ($scope) {
+        $scope.initController = function () {
+            $scope.setInitialValues();
+            $scope.setWatch();
+        };
+        $scope.setInitialValues = function () {
+        };
+        $scope.setWatch = function () {
+        };
+    })
+;
+
