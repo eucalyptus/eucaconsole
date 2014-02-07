3333b13222721aab435fe51a79a13b99057b67bc
Kyo Lee <kyolee310@gmail.com>
2014-02-06 21:19:00 -0800

    Timeout added to make the chosen widget wrapper to work with Firefox

--- koala/static/js/pages/group.js
+++ koala/static/js/pages/group.js
@@ -15,21 +15,24 @@ angular.module('GroupPage', [])
         };
         $scope.activateChosen = function () {
             $("#users-select").chosen();
-            $("#users_select_chosen .chosen-choices").bind("DOMSubtreeModified", function() {
-                var newUsers = [];
-                $("#users_select_chosen .chosen-choices .search-choice").each(function (index){
-                    var thisUser = $( this ).text();
-                    newUsers.push(thisUser);
-                });
-                var userRemoved = false;
-                if( $scope.groupUsers.length < newUsers.length ){
-                    userRemoved = true;
-                }
-                $scope.groupUsers = newUsers;
-                if( userRemoved ){
-                    $scope.$apply();
-                }
+            $("#users_select_chosen .chosen-choices").bind("DOMSubtreeModified", function(e) {
+                $timeout(function(){ $scope.adjustGroupUsers(); }, 100);
             });
+        };
+        $scope.adjustGroupUsers = function () {
+            var newUsers = [];
+            $("#users_select_chosen .chosen-choices .search-choice").each(function (index){
+                var thisUser = $( this ).text();
+                newUsers.push(thisUser);
+            });
+            var userAdded = false;
+            if( $scope.groupUsers.length < newUsers.length ){
+                userAdded = true;
+            }
+            $scope.groupUsers = newUsers;
+            if( userAdded == true ){
+                $scope.$apply();
+            }
         };
         $scope.removeUser = function (user) {
             $("#users_select_chosen .chosen-choices .search-choice").each(function (index){
