dc66a9d5257f077f8a0f02f008bdaf82ae034abb
Kamal Gill <kamal@kamalgill.net>
2014-02-12 13:56:42 -0800

    GUI-337: Hard-coded canned access policies for IAM Policy Wizard.

---
+++ koala/constants/policies.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Access policies
+
+"""
+
+API_VERSION = "2012-10-17"
+
+# Admin access policy (full access, including users/groups)
+ADMIN_ACCESS_POLICY = {
+    "Version": API_VERSION,
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+
+
+# Power user access policy (full access, except users/groups)
+USER_ACCESS_POLICY = {
+    "Version": API_VERSION,
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "NotAction": "iam:*",
+            "Resource": "*"
+        }
+    ]
+}
+
+# Monitor/readonly access policy (except users/groups)
+MONITOR_ACCES_POLICY = {
+    "Version": API_VERSION,
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:Describe*",
+                "cloudwatch:Describe*",
+                "cloudwatch:Get*",
+                "cloudwatch:List*",
+                "ec2:Describe*",
+                "s3:Get*",
+                "s3:List*",
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+
+
