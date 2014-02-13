482260686f96d2b54b83933ab89e86eeb6bd66d1
Rocky Burt <rocky@serverzen.com>
2014-02-13 15:06:27 -0330

    Added tween for setting x-frame-options header for GUI-303

---
+++ tests/tweens/test_xframe.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+class Mock(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class TestXFrame(unittest.TestCase):
+    def test_factory(self):
+        from koala.tweens import xframe_tween_factory
+        tween = xframe_tween_factory(None, None)
+        self.assertTrue(callable(tween))
+
+    def test_tween(self):
+        from koala.tweens import xframe_tween_factory
+
+        class MockHandler(object):
+            def __init__(self, content_type):
+                self.headers = {}
+                self.content_type = content_type
+
+            def __call__(self, request):
+                return Mock(content_type=self.content_type,
+                            headers=self.headers)
+
+        # make sure non html resources aren't getting header
+        tween = xframe_tween_factory(MockHandler('image/jpeg'), None)
+        res = tween(None)
+        self.assertTrue('X_FRAME_OPTIONS' not in res.headers)
+
+        # make sure html resources *are* getting header
+        tween = xframe_tween_factory(MockHandler('text/html'), None)
+        res = tween(None)
+        self.assertTrue('X_FRAME_OPTIONS' in res.headers)
+
