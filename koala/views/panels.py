c8dbb75992752ba5e2ddd5817a2dab7607092fd1
Kyo Lee <kyolee310@gmail.com>
2014-02-12 16:10:42 -0800

    Moved the macro for displaying the security group rules to the panel

--- koala/views/panels.py
+++ koala/views/panels.py
@@ -198,3 +198,9 @@ def quotas_panel(context, request, quota_form=None):
         quota_form=quota_form,
     )
 
+@panel_config('securitygroup_rules_landingpage_panel', renderer='../templates/panels/securitygroup_rules_landingpage.pt')
+def securitygroup_rules_landingpage_panel(context, request, tile_view=False):
+    print "tile_view: " , tile_view
+    return dict(
+        tile_view=tile_view,
+    )
