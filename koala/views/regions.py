842daf2ba870fede36ad540181aff235023c33cd
Kamal Gill <kamal@kamalgill.net>
2014-02-19 17:06:31 -0800

    GUI-405: Set appropriate redirect handler when the AWS region is changed.

--- koala/views/regions.py
+++ koala/views/regions.py
@@ -3,10 +3,13 @@
 Region selector view
 
 """
+from urlparse import urlparse
+
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 
 from . import BaseView
+from ..constants import LANDINGPAGE_ROUTE_NAMES
 
 
 class RegionSelectView(BaseView):
@@ -16,9 +19,14 @@ class RegionSelectView(BaseView):
     def select_region(self):
         """Select region and redirect to referring page"""
         return_to = self.request.params.get('returnto')
+        return_to_path = urlparse(return_to).path
+        landingpage_route_paths = [urlparse(self.request.route_url(name)).path for name in LANDINGPAGE_ROUTE_NAMES]
+        if return_to_path not in landingpage_route_paths:
+            return_to = self.request.route_url('dashboard')
         region = self.request.params.get('region')
         # NOTE: We normally don't want a GET request to modify data,
         #       but we're only updating the selected region in the session here.
         self.request.session['region'] = region
-        return HTTPFound(location=return_to)
+        safe_return_to = urlparse(return_to).path  # Prevent arbitrary redirects
+        return HTTPFound(location=safe_return_to)
 
