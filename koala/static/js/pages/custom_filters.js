be414b36d27fe4fc73e5a750351b5f86c9a1e7b5
Kyo Lee <kyolee310@gmail.com>
2014-02-26 12:23:22 -0800

    Added a new class "table-layout-fixed" in the macro for the landing page tables Added Angular filter "ellipsis" for the custom filter module Added the class "breakword" for the fields with potential long names -- volume and snapshot pages only for now

--- koala/static/js/pages/custom_filters.js
+++ koala/static/js/pages/custom_filters.js
@@ -3,10 +3,18 @@
    * @requires AngularJS, jQuery, and Purl jQuery URL parser plugin
 **/
 
-angular.module('CustomFilters', []).filter('escapeURL', function() {
-  return function(input) {
-    return encodeURIComponent(input);
-  };
+angular.module('CustomFilters', [])
+.filter('escapeURL', function() {
+    return function(input) {
+      return encodeURIComponent(input);
+    };
+})
+.filter('ellipsis', function () {
+    return function (line, num) {
+        if( line.length <= num ){
+            return line;
+        }
+        return line.substring(0, num) + "...";
+    };
 });
-
 
