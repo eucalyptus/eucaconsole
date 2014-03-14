471286ee610ff1143498dbace0f610a009a2f93f
Kyo Lee <kyolee310@gmail.com>
2014-03-13 23:29:10 -0700

    Added 'release' to the regex match for the dialogs

--- eucaconsole/static/js/pages/landingpage.js
+++ eucaconsole/static/js/pages/landingpage.js
@@ -80,7 +80,7 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize'])
             $(document).on('opened', '[data-reveal]', function () {
                 var modal = $(this);
                 var modalID = $(this).attr('id');
-                if( modalID.match(/terminate/)  || modalID.match(/delete/)  ){
+                if( modalID.match(/terminate/)  || modalID.match(/delete/) || modalID.match(/release/) ){
                     var closeMark = modal.find('.close-reveal-modal');
                     if(!!closeMark){
                         closeMark.focus();
