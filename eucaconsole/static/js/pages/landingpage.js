85a1e6d7204aef0d9b1d37ab21b29060fe526075
Kamal Gill <kamal@kamalgill.net>
2014-03-17 18:39:31 -0700

    Minor formatting nitpick;  remove errant semicolon

--- eucaconsole/static/js/pages/landingpage.js
+++ eucaconsole/static/js/pages/landingpage.js
@@ -102,11 +102,11 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize'])
                 modal.find('textarea').val('');
                 modal.find('div.error').removeClass('error');
                 var chosenSelect = modal.find('select');
-                if( chosenSelect.length > 0 ){
+                if (chosenSelect.length > 0) {
                     chosenSelect.chosen('destroy');
                     chosenSelect.prop('selectedIndex', 0);
                     chosenSelect.chosen();
-                };
+                }
             });
             $(document).on('closed', '[data-reveal]', function () {
                 $('#search-filter').focus();
