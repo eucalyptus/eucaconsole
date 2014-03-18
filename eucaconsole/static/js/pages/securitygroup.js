608368ac00442d06bb22d6c820558f2ed5873b28
Kyo Lee <kyolee310@gmail.com>
2014-03-18 11:24:18 -0700

    Using HTML autofocus on the create security group page Adding setFocus() via JS script for the dialogs on the security group detail page

---
+++ eucaconsole/static/js/pages/securitygroup.js
@@ -0,0 +1,36 @@
+/**
+ * @fileOverview SecurityGroup Detail Page JS
+ * @requires AngularJS
+ *
+ */
+
+angular.module('SecurityGroupPage', ['TagEditor', 'SecurityGroupRules'])
+    .controller('SecurityGroupPageCtrl', function ($scope) {
+        $scope.initController = function () {
+            $scope.setFocus();
+        };
+        $scope.setFocus = function () {
+            $(document).on('opened', '[data-reveal]', function () {
+                var modal = $(this);
+                var modalID = $(this).attr('id');
+                if( modalID.match(/terminate/)  || modalID.match(/delete/) || modalID.match(/release/) ){
+                    var closeMark = modal.find('.close-reveal-modal');
+                    if(!!closeMark){
+                        closeMark.focus();
+                    }
+                }else{
+                    var inputElement = modal.find('input[type!=hidden]').get(0);
+                    var modalButton = modal.find('button').get(0);
+                    if (!!inputElement) {
+                        inputElement.focus();
+                    } else if (!!modalButton) {
+                        modalButton.focus();
+                    }
+               }
+            });
+        };
+    })
+;
+
+
+
