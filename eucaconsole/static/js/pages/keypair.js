e7bafc6c0f6745923a4b259d0bffcabcd4443989
Kyo Lee <kyolee310@gmail.com>
2014-03-18 10:55:59 -0700

    Using HTML autofocus for the create keypair and import keypair pages Adding setFocus() for the dialogs on the keypair detail page

--- eucaconsole/static/js/pages/keypair.js
+++ eucaconsole/static/js/pages/keypair.js
@@ -24,7 +24,24 @@ angular.module('KeypairPage', [])
             });
         };
         $scope.setFocus = function () {
-            $('input#key-name').focus();
+            $(document).on('opened', '[data-reveal]', function () {
+                var modal = $(this);
+                var modalID = $(this).attr('id');
+                if( modalID.match(/terminate/)  || modalID.match(/delete/) || modalID.match(/release/) ){
+                    var closeMark = modal.find('.close-reveal-modal');
+                    if(!!closeMark){
+                        closeMark.focus();
+                    }
+                }else{
+                    var inputElement = modal.find('input[type!=hidden]').get(0);
+                    var modalButton = modal.find('button').get(0);
+                    if (!!inputElement) {
+                        inputElement.focus();
+                    } else if (!!modalButton) {
+                        modalButton.focus();
+                    }
+               }
+            });
         };
     })
 ;
