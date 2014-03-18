cedae68762618deb419fb48450b7c851b9e40de1
Kyo Lee <kyolee310@gmail.com>
2014-03-17 21:15:11 -0700

    Set focus on the scaling group wizard

--- eucaconsole/static/js/pages/scalinggroup_wizard.js
+++ eucaconsole/static/js/pages/scalinggroup_wizard.js
@@ -16,6 +16,7 @@ angular.module('ScalingGroupWizard', ['AutoScaleTagEditor'])
         $scope.urlParams = $.url().param();
         $scope.launchConfig = '';
         $scope.summarySection = $('.summary');
+        $scope.currentStepIndex = 1;
         $scope.initChosenSelectors = function () {
             $('#launch_config').chosen({'width': '80%', search_contains: true});
             $('#availability_zones').chosen({'width': '100%', search_contains: true});
@@ -30,6 +31,28 @@ angular.module('ScalingGroupWizard', ['AutoScaleTagEditor'])
             $scope.initChosenSelectors();
             $scope.setLaunchConfig();
             $scope.setInitialValues();
+            $scope.setWatcher();
+        };
+        $scope.setWatcher = function (){
+            $scope.$watch('currentStepIndex', function(){
+                 $scope.setWizardFocus($scope.currentStepIndex);
+            });
+        }
+        $scope.setWizardFocus = function (stepIdx) {
+            var modal = $('div').filter("#step" + stepIdx);
+            var inputElement = modal.find('input[type!=hidden]').get(0);
+            var textareaElement = modal.find('textarea[class!=hidden]').get(0);
+            var selectElement = modal.find('select').get(0);
+            var modalButton = modal.find('button').get(0);
+            if (!!textareaElement){
+                textareaElement.focus();
+            } else if (!!inputElement) {
+                inputElement.focus();
+            } else if (!!selectElement) {
+                selectElement.focus();
+            } else if (!!modalButton) {
+                modalButton.focus();
+            }
         };
         $scope.visitNextStep = function (nextStep, $event) {
             // Trigger form validation before proceeding to next step
@@ -44,6 +67,7 @@ angular.module('ScalingGroupWizard', ['AutoScaleTagEditor'])
             }
             // If all is well, click the relevant tab to go to next step
             $('#tabStep' + nextStep).click();
+            $scope.currentStepIndex = nextStep;
             // Unhide step 2 of summary
             if (nextStep === 2) {
                 $scope.summarySection.find('.step2').removeClass('hide');
