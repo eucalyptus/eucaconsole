d8d13efc42a6214e5da5a42aac111214282f473b
Kyo Lee <kyolee310@gmail.com>
2014-03-18 10:49:00 -0700

    Using HTML autofocus on the create volume page Adding setFocus)() for the dialogs on the volume detail page

--- eucaconsole/static/js/pages/volume.js
+++ eucaconsole/static/js/pages/volume.js
@@ -83,7 +83,24 @@ angular.module('VolumePage', ['TagEditor'])
             });
         };
         $scope.setFocus = function () {
-            $('input#name').focus();
+            $(document).on('opened', '[data-reveal]', function () {
+                var modal = $(this);
+                var modalID = $(this).attr('id');
+                if( modalID.match(/terminate/)  || modalID.match(/delete/) || modalID.match(/release/) ){
+                    var closeMark = modal.find('.close-reveal-modal');
+                    if(!!closeMark){
+                        closeMark.focus();
+                    }
+                }else{
+                    var inputElement = modal.find('input[type!=hidden]').get(0);
+                    var modalButton = modal.find('button').get(0);
+                    if (!!inputElement) {
+                        inputElement.focus();
+                    } else if (!!modalButton) {
+                        modalButton.focus();
+                    }
+               }
+            });
         };
     })
 ;
