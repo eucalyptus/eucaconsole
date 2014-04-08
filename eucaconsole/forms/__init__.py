c4d72b12cfed8e6b67527bcda3ae4224ab45b053
dkavanagh <dkavanagh@gmail.com>
2014-04-08 14:49:27 -0400

    GUI-684 added self param to get_instance_types call. Beaker seems to like at least one arg and we don't need any for this call

--- eucaconsole/forms/__init__.py
+++ eucaconsole/forms/__init__.py
@@ -94,7 +94,7 @@ class ChoicesManager(object):
             choices.append(BLANK_CHOICE)
         if cloud_type == 'euca':
             @cache_region('extra_long_term', 'instance_types')
-            def _get_instance_types_cache():
+            def _get_instance_types_cache(self):
                 choices = []
                 if self.conn is not None:
                     types = self.conn.get_all_instance_types()
@@ -104,7 +104,7 @@ class ChoicesManager(object):
                         vmtype_tuple = vmtype.name, vmtype_str if add_description else vmtype.name
                         choices.append(vmtype_tuple)
                 return choices
-            choices.extend(_get_instance_types_cache())
+            choices.extend(_get_instance_types_cache(self))
             return choices
         elif cloud_type == 'aws':
             return choices.extend(AWS_INSTANCE_TYPE_CHOICES)
