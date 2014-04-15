9f71dbd4bfd94541b8bd02ea5069736c837b28c6
Kyo Lee <kyolee310@gmail.com>
2014-04-15 00:38:00 -0700

    Removing the previous commit which didn't fix the unitest

--- tests/images/test_images.py
+++ tests/images/test_images.py
@@ -5,7 +5,6 @@ See http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/testing.html
 
 """
 from pyramid import testing
-from pyramid.httpexceptions import HTTPNotFound
 
 from eucaconsole.forms.images import ImageForm
 from eucaconsole.views import TaggedItemView
