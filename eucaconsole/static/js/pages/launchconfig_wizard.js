dc711e997e4c78cadb0d4c3f83b41b1bbde07fb8
Kamal Gill <kamal@kamalgill.net>
2014-04-28 13:28:06 -0700

    GUI-837: Prevent launch instance and launch config wizards from breaking when a security group or key pair has a single quote in the name

--- eucaconsole/static/js/pages/launchconfig_wizard.js
+++ eucaconsole/static/js/pages/launchconfig_wizard.js
@@ -40,6 +40,10 @@ angular.module('LaunchConfigWizard', ['ImagePicker', 'BlockDeviceMappingEditor',
         $scope.initController = function (securityGroupsRulesJson, keyPairChoices,
                                 securityGroupChoices, securityGroupsIDMapJson,
                                 imageJsonURL) {
+            securityGroupsRulesJson = securityGroupsRulesJson.replace(/__apos__/g, "\'");
+            securityGroupChoices = securityGroupChoices.replace(/__apos__/g, "\'");
+            securityGroupsIDMapJson = securityGroupsIDMapJson.replace(/__apos__/g, "\'");
+            keyPairChoices = keyPairChoices.replace(/__apos__/g, "\'");
             $scope.securityGroupsRules = JSON.parse(securityGroupsRulesJson);
             $scope.keyPairChoices = JSON.parse(keyPairChoices);
             $scope.securityGroupChoices = JSON.parse(securityGroupChoices);
