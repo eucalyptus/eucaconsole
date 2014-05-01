ebf592b4763dd579c4294d276a44c084de257563
Rocky Burt <rocky@serverzen.com>
2014-05-01 09:49:12 -0230

    Added tests for https tween

--- tests/tweens/test_tweens.py
+++ tests/tweens/test_tweens.py
@@ -14,6 +14,21 @@ class MockConfig(Mock):
         self.tweens.append(mpath)
 
 
+class MockHandler(object):
+    def __init__(self, content_type=None):
+        self.headers = {}
+        self.content_type = content_type
+
+    def __call__(self, request):
+        return Mock(content_type=self.content_type,
+                    headers=self.headers)
+
+
+class MockRequest(object):
+    def __init__(self):
+        self.environ = {}
+
+
 class TestSetup(unittest.TestCase):
     def test_it(self):
         from eucaconsole.tweens import setup_tweens
@@ -35,15 +50,6 @@ class TestCTHeaders(unittest.TestCase):
         from eucaconsole.tweens import \
             CTHeadersTweenFactory as factory
 
-        class MockHandler(object):
-            def __init__(self, content_type):
-                self.headers = {}
-                self.content_type = content_type
-
-            def __call__(self, request):
-                return Mock(content_type=self.content_type,
-                            headers=self.headers)
-
         tween = factory(MockHandler('image/jpeg'), None)
         res = tween(None)
         for name, value in factory.header_map['text/html'].items():
@@ -56,3 +62,19 @@ class TestCTHeaders(unittest.TestCase):
             # make sure html resources *are* getting header
             self.assertTrue(name in res.headers)
             self.assertTrue(res.headers[name] == value)
+
+
+class TestHTTPSTween(unittest.TestCase):
+    def test_it(self):
+        from eucaconsole.tweens import \
+            https_tween_factory as factory
+        tween = factory(MockHandler(), None)
+
+        request = Mock(scheme=None, environ={})
+        tween(request)
+        self.assertTrue(request.scheme is None)
+
+        request = Mock(scheme=None,
+                       environ={'HTTP_X_FORWARDED_PROTO': 'https'})
+        tween(request)
+        self.assertEqual(request.scheme, 'https')
