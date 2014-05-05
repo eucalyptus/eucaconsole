b6b0d9c8766ec7025511277af1e5b42ea3753b42
Kamal Gill <kamal@kamalgill.net>
2014-05-05 14:56:51 -0700

    GUI-926: Minor fixes for conditional display of available groups drop-down menu.

--- eucaconsole/static/js/pages/user_view.js
+++ eucaconsole/static/js/pages/user_view.js
@@ -395,6 +395,8 @@ angular.module('UserView', ['PolicyList'])
         $scope.policyName = '';
         $scope.policyJson = '';
         $scope.noAvailableGroups = false;
+        $scope.alreadyMemberOfAllGroups = false;
+        $scope.noGroupsDefined = false;
         $scope.initController = function (addEndpoint, removeEndpoint, jsonItemsEndpoint, jsonGroupsEndpoint,
                                           jsonGroupPoliciesEndpoint, jsonGroupPolicyEndpoint, alreadyMemberText, noGroupsDefinedText) {
             $scope.addEndpoint = addEndpoint;
@@ -439,10 +441,10 @@ angular.module('UserView', ['PolicyList'])
             $http.get($scope.jsonGroupsEndpoint).success(function(oData) {
                 var results = oData ? oData.results : [];
                 $scope.groupNames = results;
-                $scope.noAvailableGroups = results.indexOf($scope.alreadyMemberText) !== -1 || results.indexOf($scope.noGroupsDefinedText) !== -1;
-                if (!$scope.noAvailableGroups) {
-                    $('#group_name').val('');
-                }
+                $scope.alreadyMemberOfAllGroups = results.indexOf($scope.alreadyMemberText) !== -1;
+                $scope.noGroupsDefined = results.indexOf($scope.noGroupsDefinedText) !== -1;
+                $scope.noAvailableGroups = $scope.alreadyMemberOfAllGroups || $scope.noGroupsDefined;
+                $scope.groupName = '';
             }).error(function (oData, status) {
                 var errorMsg = oData['message'] || '';
                 if (errorMsg && status === 403) {
