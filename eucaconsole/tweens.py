9b76772f25bbb99c4800802a6333813ed122115e
Kamal Gill <kamal@kamalgill.net>
2014-05-07 11:34:35 -0700

    GUI-939: Restore request.id tween, using random instead of os.urandom

--- eucaconsole/tweens.py
+++ eucaconsole/tweens.py
@@ -23,12 +23,17 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import string
+
+from random import choice
+
 
 def setup_tweens(config):
     """Since tweens order is important this function will
     take care of proper ordering"""
 
     config.add_tween('eucaconsole.tweens.https_tween_factory')
+    config.add_tween('eucaconsole.tweens.request_id_tween_factory')
     config.add_tween('eucaconsole.tweens.CTHeadersTweenFactory')
 
 
@@ -37,6 +42,15 @@ def https_tween_factory(handler, registry):
         response = handler(request)
         if request.environ.get('HTTP_X_FORWARDED_PROTO') == 'https':
             request.scheme = 'https'
+        return response
+    return tween
+
+
+def request_id_tween_factory(handler, registry):
+    def tween(request):
+        chars = string.letters + string.digits
+        request.id = ''.join(choice(chars) for i in range(16))
+        response = handler(request)
         return response
     return tween
 
