1328556975ce9966fa72ab77c01159174c429a5f
dkavanagh <dkavanagh@gmail.com>
2014-05-12 13:22:32 -0400

    GUI-263 fixed error message for session timeout

--- eucaconsole/views/__init__.py
+++ eucaconsole/views/__init__.py
@@ -194,7 +194,7 @@ class BaseView(object):
             if any(['Invalid access key' in message, 'Invalid security token' in message]):
                 notice = message
             else:
-                notice = _(u'Your session has timed out')
+                notice = _(u'Your session has timed out. This may be due to inactivity, a policy that does not provide login permissions, or an unexpected error. Please log in again, and contact your cloud administrator if the problem persists.')
             request.session.flash(notice, queue=Notification.WARNING)
             # Empty Beaker cache to clear connection objects
             # BaseView.invalidate_connection_cache()
