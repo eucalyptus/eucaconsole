1b775263f43c22f8fe7705620010c538660bf304
Kyo Lee <kyolee310@gmail.com>
2014-06-10 12:49:41 -0700

    Adjusted the beforeunload handler in case of the submit button is pressed on the scaling group detail page

--- eucaconsole/static/js/pages/scalinggroup.js
+++ eucaconsole/static/js/pages/scalinggroup.js
@@ -11,6 +11,7 @@ angular.module('ScalingGroupPage', ['AutoScaleTagEditor'])
         $scope.desiredCapacity = 1;
         $scope.maxSize = 1;
         $scope.isNotChanged = true;
+        $scope.isSubmitted = false;
         $scope.initChosenSelectors = function () {
             $('#launch_config').chosen({'width': '60%', search_contains: true});
             $('#availability_zones').chosen({'width': '80%', search_contains: true});
@@ -57,6 +58,9 @@ angular.module('ScalingGroupPage', ['AutoScaleTagEditor'])
                 $scope.isNotChanged = false;
                 $scope.$apply();
             });
+            $(document).on('submit', function () {
+                $scope.isSubmitted = true;
+            });
             window.addEventListener("beforeunload", function(event) {
                 var existsUnsavedTag = false;
                 $('input.taginput[type!="checkbox"]').each(function(){
@@ -68,7 +72,7 @@ angular.module('ScalingGroupPage', ['AutoScaleTagEditor'])
                 if(existsUnsavedTag){
                     return "You must click the \"Add\" button before you submit this for your tag to be included.";
                 }else if($scope.isNotChanged === false){
-                    if( event.target.activeElement.id === 'save-changes-btn' ){ 
+                    if( $scope.isSubmitted === true ){
                         return;
                     }
                     return "You must click the \"Save Changes\" button before you leave this page.";
