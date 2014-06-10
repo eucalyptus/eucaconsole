b92bfce272e551adc510676bc532216fc70d8fb6
Kyo Lee <kyolee310@gmail.com>
2014-06-10 12:56:43 -0700

    Adjusted the beforeunload handler in case of the submit event on the security group detail page

--- eucaconsole/static/js/pages/securitygroup.js
+++ eucaconsole/static/js/pages/securitygroup.js
@@ -7,6 +7,7 @@
 angular.module('SecurityGroupPage', ['TagEditor', 'SecurityGroupRules'])
     .controller('SecurityGroupPageCtrl', function ($scope) {
         $scope.isNotChanged = true;
+        $scope.isSubmitted = false;
         $scope.initController = function () {
             $scope.setWatch();
             $scope.setFocus();
@@ -22,6 +23,9 @@ angular.module('SecurityGroupPage', ['TagEditor', 'SecurityGroupRules'])
                     }
                 });
             });
+            $(document).on('submit', function () {
+                $scope.isSubmitted = true;
+            });
             window.addEventListener("beforeunload", function(event) {
                 var existsUnsavedTag = false;
                 $('input.taginput').each(function(){
@@ -32,7 +36,7 @@ angular.module('SecurityGroupPage', ['TagEditor', 'SecurityGroupRules'])
                 if(existsUnsavedTag){
                     return "You must click the \"Add\" button before you submit this for your tag to be included.";
                 }else if($scope.isNotChanged === false){
-                    if( event.target.activeElement.id === 'save-securitygroup-btn' ){ 
+                    if( $scope.isSubmitted === true ){
                         return;
                     }
                     return "You must click the \"Save Changes\" button before you leave this page.";
