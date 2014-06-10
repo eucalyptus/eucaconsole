cbb29d519f79102f5deb769edea034b4cfc45696
Kyo Lee <kyolee310@gmail.com>
2014-06-10 00:00:09 -0700

    Added more condition on the 'beforeunload' case on the instance detail page

--- eucaconsole/static/js/pages/instance.js
+++ eucaconsole/static/js/pages/instance.js
@@ -106,6 +106,11 @@ angular.module('InstancePage', ['TagEditor'])
                 });
                 if(existsUnsavedTag){
                     return "You must click the \"Add\" button before you submit this for your tag to be included.";
+                }else if($scope.isNotChanged === false){
+                    if( event.target.activeElement.id === 'save-changes-btn' ){ 
+                        return;
+                    }
+                    return "You must click the \"Save Changes\" button before you leave this page.";
                 }
             });
         };
