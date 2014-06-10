63590f5e13bee39cf92ecb5fa3644eb4facbc94d
Kyo Lee <kyolee310@gmail.com>
2014-06-09 23:39:40 -0700

    Added the 'beforeunload' listener to the instance detail page

--- eucaconsole/static/js/pages/instance.js
+++ eucaconsole/static/js/pages/instance.js
@@ -97,6 +97,17 @@ angular.module('InstancePage', ['TagEditor'])
                 $(this).find('.dialog-submit-button').css('display', 'none');                
                 $(this).find('.dialog-progress-display').css('display', 'block');                
             });
+            window.addEventListener("beforeunload", function(event) {
+                var existsUnsavedTag = false;
+                $('input.taginput').each(function(){
+                    if($(this).val() !== ''){
+                        existsUnsavedTag = true;
+                    }
+                });
+                if(existsUnsavedTag){
+                    return "You must click the \"Add\" button before you submit this for your tag to be included.";
+                }
+            });
         };
         $scope.getIPAddressData = function () {
             $http.get($scope.instanceIPAddressEndpoint).success(function(oData) {
