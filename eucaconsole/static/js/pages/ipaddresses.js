a9fd2f175705fff6ca5ba11e6333284f1ac4c155
Kyo Lee <kyolee310@gmail.com>
2014-06-18 10:29:22 -0700

    Adjusting the input listener on the allocate IPs dialog to work with Selenium test running on FireFox

--- eucaconsole/static/js/pages/ipaddresses.js
+++ eucaconsole/static/js/pages/ipaddresses.js
@@ -23,9 +23,20 @@ angular.module('ElasticIPsPage', ['LandingPage'])
             $scope.setWatch();
         };
         $scope.setWatch = function () {
-            $(document).on('input', '#ipcount', function () {
-                $scope.isNotValid = false;
-                $scope.$apply();
+            $(document).on('opened', '[data-reveal]', function () {
+                $(document).on('input', '#ipcount', function () {
+                    if( $scope.isNotValid === true ){
+                        $scope.isNotValid = false;
+                        $scope.$apply();
+                    }
+                });
+                // For FireFox Selenium Test, which triggers "change" event when type input on the number inputbox.
+                $(document).on('change', '#ipcount', function () {
+                    if( $scope.isNotValid === true ){
+                        $scope.isNotValid = false;
+                        $scope.$apply();
+                    }
+                });
             });
             $(document).on('close', '[data-reveal]', function () {
                 $scope.isNotValid = true;
