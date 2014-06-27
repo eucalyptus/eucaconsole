75737340e509f0492ed8f67cb57a56a24f0bd8d2
Kamal Gill <kamal@kamalgill.net>
2014-06-27 16:02:08 -0700

    GUI-856: Display warning when an IAM policy is edited by hand in the policy generator

--- eucaconsole/static/js/pages/iam_policy_wizard.js
+++ eucaconsole/static/js/pages/iam_policy_wizard.js
@@ -24,6 +24,7 @@ angular.module('IAMPolicyWizard', [])
         $scope.languageCode = 'en';
         $scope.confirmed = false;
         $scope.isCreating = false;
+        $scope.handEdited = false;
         $scope.nameConflictKey = 'doNotShowPolicyNameConflictWarning';
         $scope.initController = function (options, save_url) {
             $scope.policyJsonEndpoint = options['policyJsonEndpoint'];
@@ -62,6 +63,7 @@ angular.module('IAMPolicyWizard', [])
                 $scope.initToggleAdvancedListener();
                 $scope.initSelectActionListener();
                 $scope.initNameConflictWarningListener();
+                $scope.initHandEditedWarningListener();
             });
         };
         $scope.initToggleAdvancedListener = function () {
@@ -80,6 +82,13 @@ angular.module('IAMPolicyWizard', [])
                     }
                 }
                 $scope.savePolicy();
+            });
+        };
+        $scope.initHandEditedWarningListener = function () {
+            $scope.codeEditor.on('change', function () {
+                $scope.$apply(function() {
+                    $scope.handEdited = $scope.codeEditor.getValue().trim() != $scope.policyText;
+                });
             });
         };
         $scope.confirmWarning = function () {
@@ -242,6 +251,7 @@ angular.module('IAMPolicyWizard', [])
             $scope.setPolicyName(policyType);
         };
         $scope.updatePolicy = function() {
+            $scope.handEdited = false;
             $scope.policyStatements = [];
             // Add namespace (allow/deny all) statements
             $scope.policyGenerator.find('tr.namespace').each(function (idx, elem) {
