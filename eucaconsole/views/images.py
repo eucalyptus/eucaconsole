6c3d4a0a280b1466dd3feb575b7d75af235f894d
Kyo Lee <kyolee310@gmail.com>
2014-07-10 13:41:21 -0700

    Allowe image description to be updated only for the images owned by user

--- eucaconsole/views/images.py
+++ eucaconsole/views/images.py
@@ -295,30 +295,31 @@ class ImageView(TaggedItemView):
         if self.image_form.validate():
             self.update_tags()
 
-            # Update the Image to be Public
-            # Note. The order of operation matters
-            # On Euca, when the group launch permissions are changed, it deletes the description
-            is_public = self.request.params.get('sharing')
-            current_is_public = str(self.image.is_public).lower()
-            if is_public != current_is_public:
-                lp_params = {}
-                if is_public == "true":
-                    lp_params = { 'ImageId': self.image.id, 'LaunchPermission.Add.1.Group': 'all' }
-                else:
-                    lp_params = { 'ImageId': self.image.id, 'LaunchPermission.Remove.1.Group': 'all' }
-                self.conn.get_status('ModifyImageAttribute', lp_params, verb='POST')
+            if self.is_owned_by_user is True: 
+                # Update the Image to be Public
+                # Note. The order of operation matters
+                # On Euca, when the group launch permissions are changed, it deletes the description
+                is_public = self.request.params.get('sharing')
+                current_is_public = str(self.image.is_public).lower()
+                if is_public != current_is_public:
+                    lp_params = {}
+                    if is_public == "true":
+                        lp_params = { 'ImageId': self.image.id, 'LaunchPermission.Add.1.Group': 'all' }
+                    else:
+                        lp_params = { 'ImageId': self.image.id, 'LaunchPermission.Remove.1.Group': 'all' }
+                    self.conn.get_status('ModifyImageAttribute', lp_params, verb='POST')
 
-            # Update the Image Description
-            description = self.request.params.get('description', '')
-            if self.image.description != description:
-                if self.cloud_type == 'aws' and description == '':
-                    description = "-"
-                params = { 'ImageId': self.image.id, 'Description.Value': description }
-                self.conn.get_status('ModifyImageAttribute', params, verb='POST')
+                # Update the Image Description
+                description = self.request.params.get('description', '')
+                if self.image.description != description:
+                    if self.cloud_type == 'aws' and description == '':
+                        description = "-"
+                    params = { 'ImageId': self.image.id, 'Description.Value': description }
+                    self.conn.get_status('ModifyImageAttribute', params, verb='POST')
 
-            # Update the Image Launch Permissions
-            lp_array = self.request.params.getall('launch-permissions-inputbox')
-            self.image_update_launch_permissions(lp_array)
+                # Update the Image Launch Permissions
+                lp_array = self.request.params.getall('launch-permissions-inputbox')
+                self.image_update_launch_permissions(lp_array)
 
             # Clear images cache
             ImagesView.invalidate_images_cache()
