ee5dd98fc22f16201bd4643c01048c58603258e0
Kyo Lee <kyolee310@gmail.com>
2014-07-28 01:10:19 -0700

    Remove $location service on the volume detail page, which was left-out from the previous adjustment

--- eucaconsole/static/js/pages/volume.js
+++ eucaconsole/static/js/pages/volume.js
@@ -6,10 +6,7 @@
 
 // Volume page includes the tag editor, so pull in that module as well.
 angular.module('VolumePage', ['TagEditor'])
-    .config(function($locationProvider) {
-        $locationProvider.html5Mode(true);
-    })
-    .controller('VolumePageCtrl', function ($scope, $http, $timeout, $location) {
+    .controller('VolumePageCtrl', function ($scope, $http, $timeout) {
         $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         $scope.volumeStatusEndpoint = '';
         $scope.transitionalStates = ['creating', 'deleting', 'attaching', 'detaching'];
