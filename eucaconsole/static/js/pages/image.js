ff2625a5520c679bfe1fcd63894385787f8d9de7
Kyo Lee <kyolee310@gmail.com>
2014-08-14 14:20:50 -0700

    Added the unsaved changes warning modal change to the image detail page

--- eucaconsole/static/js/pages/image.js
+++ eucaconsole/static/js/pages/image.js
@@ -29,6 +29,9 @@ angular.module('ImagePage', ['BlockDeviceMappingEditor', 'TagEditor'])
             $scope.setWatch();
             $scope.setFocus();
         };
+        $scope.isTransitional = function (state) {
+            return $scope.transitionalStates.indexOf(state) !== -1;
+        };
         // True if there exists an unsaved key or value in the tag editor field
         $scope.existsUnsavedTag = function () {
             var hasUnsavedTag = false;
@@ -39,10 +42,42 @@ angular.module('ImagePage', ['BlockDeviceMappingEditor', 'TagEditor'])
             });
             return hasUnsavedTag;
         };
-        $scope.isTransitional = function (state) {
-            return $scope.transitionalStates.indexOf(state) !== -1;
+        $scope.openModalById = function (modalID) {
+            var modal = $('#' + modalID);
+            modal.foundation('reveal', 'open');
+            modal.find('h3').click();  // Workaround for dropdown menu not closing
+            // Clear the pending modal ID if opened
+            if ($scope.pendingModalID === modalID) {
+                $scope.pendingModalID = '';
+            }
         };
         $scope.setWatch = function () {
+            // Monitor the action menu click
+            $(document).on('click', 'a[id$="action"]', function (event) {
+                // Ingore the action if the link has a ng-click attribute
+                if (this.getAttribute('ng-click')) {
+                    return;
+                } else if (this.getAttribute('href') && this.getAttribute('href') !== '#') {
+                    return;
+                }
+                // the ID of the action link needs to match the modal name
+                var modalID = this.getAttribute('id').replace("-action", "-modal");
+                // If there exists unsaved changes, open the wanring modal instead
+                if ($scope.existsUnsavedTag() || $scope.isNotChanged === false) {
+                    $scope.pendingModalID = modalID;
+                    $scope.openModalById('unsaved-changes-warning-modal');
+                    return;
+                } 
+                $scope.openModalById(modalID);
+            });
+            // Leave button is clicked on the warning unsaved changes modal
+            $(document).on('click', '#unsaved-changes-warning-modal-stay-button', function () {
+                $('#unsaved-changes-warning-modal').foundation('reveal', 'close');
+            });
+            // Stay button is clicked on the warning unsaved changes modal
+            $(document).on('click', '#unsaved-changes-warning-modal-leave-link', function () {
+                $scope.openModalById($scope.pendingModalID);
+            });
             $scope.$on('tagUpdate', function($event) {
                 $scope.isNotChanged = false;
             });
@@ -70,24 +105,6 @@ angular.module('ImagePage', ['BlockDeviceMappingEditor', 'TagEditor'])
             // Do not perfom the unsaved changes check if the cancel link is clicked
             $(document).on('click', '.cancel-link', function(event) {
                 window.onbeforeunload = null;
-            });
-            // Handle the case when user tries to open a dialog while there exist unsaved changes
-            $(document).on('open', '[data-reveal][id!="unsaved-changes-warning-modal"][id!="unsaved-tag-warn-modal"]', function () {
-                // If there exist unsaved changes
-                if ($scope.existsUnsavedTag() || $scope.isNotChanged === false) {
-                    var self = this;
-                    // Close the current dialog as soon as it opens
-                    $(self).on('opened', function() {
-                        $(self).off('opened');
-                        $(self).foundation('reveal', 'close');
-                    });
-                    // Open the warning message dialog instead
-                    $(self).on('closed', function() {
-                        $(self).off('closed');
-                        var modal = $('#unsaved-changes-warning-modal');
-                        modal.foundation('reveal', 'open');
-                    });
-                } 
             });
             $scope.$watch('isPublic', function(newValue, oldValue) {
                 if (newValue !== oldValue) {
