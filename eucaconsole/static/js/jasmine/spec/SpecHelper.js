80382abdb3b0d176bde658bb38d8e3afac140e44
Kyo Lee <kyolee310@gmail.com>
2014-10-15 16:10:55 -0700

    Removed unused contents that came with the installation of Jasmine 2.0.3

--- eucaconsole/static/js/jasmine/spec/SpecHelper.js
+++ eucaconsole/static/js/jasmine/spec/SpecHelper.js
@@ -1,16 +1,5 @@
 beforeEach(function () {
   jasmine.addMatchers({
-    toBePlaying: function () {
-      return {
-        compare: function (actual, expected) {
-          var player = actual;
-
-          return {
-            pass: player.currentlyPlayingSong === expected && player.isPlaying
-          }
-        }
-      };
-    },
     toBeEmptyArray: function () {
       return {
         compare: function (actual) {
