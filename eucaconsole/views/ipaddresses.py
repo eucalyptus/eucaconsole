2097673579bcaf1c4bf4aef49b957491d708402b
dkavanagh <dkavanagh@gmail.com>
2014-10-31 13:31:20 -0400

    GUI-1404 pass allocation_id for release VPC address operation

--- eucaconsole/views/ipaddresses.py
+++ eucaconsole/views/ipaddresses.py
@@ -127,9 +127,13 @@ class IPAddressesView(LandingPageView):
     def ipaddresses_release(self):
         if self.release_form.validate():
             public_ip = self.request.params.get('public_ip')
+            allocation_id = self.request.params.get('allocation_id')
             with boto_error_handler(self.request, self.location):
                 self.log_request(_(u"Releasing ElasticIP {0}").format(public_ip))
-                self.conn.release_address(public_ip)
+                if allocation_id == '':
+                    self.conn.release_address(public_ip=public_ip)
+                else:
+                    self.conn.release_address(allocation_id=allocation_id)
                 template = _(u'Successfully released {ip} to the cloud')
                 msg = template.format(ip=public_ip)
                 self.request.session.flash(msg, queue=Notification.SUCCESS)
@@ -171,6 +175,7 @@ class IPAddressesJsonView(LandingPageView):
             for address in items:
                 ipaddresses.append(dict(
                     public_ip=address.public_ip,
+                    allocation_id=address.allocation_id,
                     instance_id=address.instance_id,
                     instance_name=TaggedItemView.get_display_name(
                         instances[address.instance_id]) if address.instance_id else address.instance_id,
