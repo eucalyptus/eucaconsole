f703ebd5759b42e20103263aacbcb3aafbd069fc
Kyo Lee <kyolee310@gmail.com>
2014-12-19 12:23:51 -0800

    Added keypairs JS unit-test

--- eucaconsole/static/js/jasmine-spec/spec_keypair.js
+++ eucaconsole/static/js/jasmine-spec/spec_keypair.js
@@ -20,6 +20,14 @@ describe("KeypairPage", function() {
         });
     }));
 
+    beforeEach(function() {
+        var template = window.__html__['templates/keypairs/keypair_view.pt'];
+        // remove <script src> and <link> tags to avoid phantomJS error
+        template = template.replace(/script src/g, "script ignore_src"); 
+        template = template.replace(/\<link/g, "\<ignore_link"); 
+        setFixtures(template);
+    });
+
     describe("Initial Values Test", function() {
 
         it("Initial value of isNotValid is true", function() {
