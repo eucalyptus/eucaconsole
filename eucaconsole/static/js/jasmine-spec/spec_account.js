c53d3a9617163f68f4ac0b35f57cb14f021edfa2
Kyo Lee <kyolee310@gmail.com>
2015-02-12 10:07:45 -0800

    Fixed the grunt jshint issues on account unit-test

--- eucaconsole/static/js/jasmine-spec/spec_account.js
+++ eucaconsole/static/js/jasmine-spec/spec_account.js
@@ -24,7 +24,7 @@ describe("AccountPage", function() {
         var template = window.__html__['templates/accounts/account_view.pt'];
         // remove <script src> and <link> tags to avoid phantomJS error
         template = template.replace(/script src/g, "script ignore_src"); 
-        template = template.replace(/\<link/g, "\<ignore_link"); 
+        template = template.replace(/<link/g, "<ignore_link"); 
         setFixtures(template);
     });
 
