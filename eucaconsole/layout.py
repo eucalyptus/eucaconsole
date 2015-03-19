44005150f482e0a8f13d3a9cad4606fbdb832f70
dkavanagh <dkavanagh@gmail.com>
2015-03-19 13:44:23 -0700

    GUI-1472 updated some search bar behaviors

--- eucaconsole/layout.py
+++ eucaconsole/layout.py
@@ -93,7 +93,7 @@ class MasterLayout(object):
         self.file_uploads_enabled = asbool(self.request.registry.settings.get('file.uploads.enabled', True))
         self.searchtext_remove = _(u'Remove facet') 
         self.searchtext_cancel = _(u'Clear search') 
-        self.searchtext_prompt = _(u'Select facets for filter, or enter text to search') 
+        self.searchtext_prompt = _(u'Select facets to filter, or enter text to search') 
         self.searchtext_text_facet = _(u'Text') 
 
     def get_notifications(self):
