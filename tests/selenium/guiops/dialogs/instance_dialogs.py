3b1cef6b1b16dc7c60310bff6a1fefc0de0e05d9
aliceh <alice@eucalyptus.com>
2015-05-24 15:48:33 -0700

    Adding lainch instance test.

--- tests/selenium/guiops/dialogs/instance_dialogs.py
+++ tests/selenium/guiops/dialogs/instance_dialogs.py
@@ -15,7 +15,8 @@ class LaunchInstanceWidget(BaseDialog):
     _keypair_selector_id = "keypair"
     _security_group_selector_id = "securitygroup_chosen"
     _security_group_choices_css = "ul.chosen-choices"
-    _security_group_search_field_css = "ul.chosen-choices>li.search-field"
+    _security_group_search_field_css = "ul.chosen-choices>li.search-field>input"
+    _highlighted_security_group_css = "[class = 'active-result highlighted']"
     _launch_instance_button_id = "launch-instance-btn-step3"
 
     def launch_centos_instance(self, availability_zone = None, instance_type = "t1.micro: 1 CPUs, 256 memory (MB), 5 disk (GB,root device)", number_of_of_instances = None, name = None, key_name = "None (advanced option)", security_group = "default"):
@@ -33,6 +34,7 @@ class LaunchInstanceWidget(BaseDialog):
         self.tester.select_by_id(self._keypair_selector_id, key_name)
         self.tester.click_element_by_css(self._security_group_choices_css)
         self.tester.send_keys_by_css(self._security_group_search_field_css, security_group)
+        self.tester.click_element_by_css(self._highlighted_security_group_css)
         self.tester.click_element_by_id(self._launch_instance_button_id)
 
 
