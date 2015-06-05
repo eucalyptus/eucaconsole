883c1b5cf6535be9fa003af8fb7706b7859400a4
aliceh <alice@eucalyptus.com>
2015-06-05 14:32:50 -0700

    Added content to image view page.

--- tests/selenium/guiops/pages/image/image_view.py
+++ tests/selenium/guiops/pages/image/image_view.py
@@ -6,7 +6,15 @@ class ImageView(ViewPage):
         self.tester = tester
         self.verify_image_view_page_loaded()
 
-    _
+    _image_view_page_title = "Images"
+    _image_action_menu_css = "[data-dropdown='item-dropdown_{0}']"  #image_id required
+    _view_details_actions_menu_item_css ="#item-dropdown_{0}>li:nth-of-type(1)>a" #image_id required
+    _launch_instance_actions_menu_item_css ="#item-dropdown_{0}>li:nth-of-type(4)>a" #image_id required
 
     def verify_image_view_page_loaded(self):
-        pass
+        self.tester.wait_for_text_present_by_id(ViewPage(self)._page_title_id, self._image_view_page_title)
+        self.tester.wait_for_visible_by_id(ViewPage(self)._refresh_button_id)
+
+    def click_action_launch_instance(self, image_id):
+        self.tester.click_element_by_css(self._image_action_menu_css.format(image_id))
+        self.tester.click_element_by_css(self._launch_instance_actions_menu_item_css.format(image_id))
