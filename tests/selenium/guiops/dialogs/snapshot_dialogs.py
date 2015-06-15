f18330b69abd5f103b0b44d94592342a09a38932
aliceh <alice@eucalyptus.com>
2015-06-15 16:10:19 -0700

    Added create_snapshot_modal and delete_snapshot_modal

---
+++ tests/selenium/guiops/dialogs/snapshot_dialogs.py
@@ -0,0 +1,28 @@
+from pages.basepage import BasePage
+
+class CreateSnapshotModal(BasePage):
+
+    def __init__(self, tester):
+        self.tester = tester
+
+    _snapshot_name_field_id = "name"
+    _snapshot_description_field_id = "description"
+    _create_snapshot_submit_button_css = '[class="button expand"]'
+
+    def create_snapshot(self, snapshot_name=None, snapshot_description=None):
+        if snapshot_name is not None:
+            self.tester.send_keys_by_id(self._snapshot_name_field_id, snapshot_name)
+        if snapshot_description is not None:
+            self.tester.send_keys_by_id(self._snapshot_description_field_id, snapshot_description)
+        self.tester.click_element_by_css(self._create_snapshot_submit_button_css)
+
+
+class DeleteSnapshotModal(BasePage):
+
+    def __init__(self, tester):
+        self.tester = tester
+
+    _delete_snapshot_submit_button_id = "delete_snapshot_submit_button"
+
+    def delete_snapshot(self):
+        self.tester.click_element_by_id(self._delete_snapshot_submit_button_id)
