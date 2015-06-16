cdce923544ade28504e1dd837a95505e9fa6433d
aliceh <alice@eucalyptus.com>
2015-06-16 11:12:55 -0700

    Changed test locators to xpath in CreateSnapshotModal due being unable to access by id

--- tests/selenium/guiops/dialogs/snapshot_dialogs.py
+++ tests/selenium/guiops/dialogs/snapshot_dialogs.py
@@ -13,15 +13,15 @@ class CreateSnapshotModal(BasePage):
     def __init__(self, tester):
         self.tester = tester
 
-    _snapshot_name_field_id = "name"
-    _snapshot_description_field_id = "description"
+    _snapshot_name_field_xpath = "(//input[@id='name'])[2]"
+    _snapshot_description_field_xpath = "(//textarea[@id='description'])[2]"
     _create_snapshot_submit_button_css = '[class="button expand"]'
 
     def create_snapshot(self, snapshot_name=None, snapshot_description=None):
         if snapshot_name is not None:
-            self.tester.send_keys_by_id(self._snapshot_name_field_id, snapshot_name)
+            self.tester.send_keys_by_xpath(self._snapshot_name_field_xpath, snapshot_name)
         if snapshot_description is not None:
-            self.tester.send_keys_by_id(self._snapshot_description_field_id, snapshot_description)
+            self.tester.send_keys_by_xpath(self._snapshot_description_field_xpath, snapshot_description)
         self.tester.click_element_by_css(self._create_snapshot_submit_button_css)
 
 
