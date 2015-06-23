d531f96b1b8aede8b9550891b5068055030f4b93
aliceh <alice@eucalyptus.com>
2015-06-23 16:49:19 -0700

    adding parser

--- tests/selenium/guiops/tests/__init__.py
+++ tests/selenium/guiops/tests/__init__.py
@@ -1,1 +1,6 @@
 __author__ = 'alicehubenko'
+
+import argparse
+
+class  Options argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True)
+
