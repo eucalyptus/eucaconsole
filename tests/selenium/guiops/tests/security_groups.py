325fd48f9e92d4a6a3aeb21cfeebc9c5107767d3
aliceh <alice@eucalyptus.com>
2015-07-06 16:07:13 -0700

    Fixed create_s_group_with_rules method

--- tests/selenium/guiops/tests/security_groups.py
+++ tests/selenium/guiops/tests/security_groups.py
@@ -22,17 +22,17 @@ class Security_group_operations_sequence(GuiEC2):
     def security_group_ops_test(self):
 
         self.tester.login("ui-test-acct-00", "admin", "mypassword0")
-        s_group_name_1 = self.id_generator() + "_test_group"
-        group1 = self.tester.create_security_group_from_dashboard(s_group_name_1, self.s_group_description)
-        s_group_id_1 = group1.get("s_group_id")
-        self.tester.add_tcp_22_rule_to_s_group(s_group_name_1, s_group_id_1)
-        self.tester.add_custom_tcp_rule_to_s_group(s_group_name_1, s_group_id_1)
-        self.tester.delete_security_group_from_detail_page(s_group_name_1, s_group_id_1)
-        s_group_name_2 = self.id_generator() + "_test_group"
-        group2 = self.tester.create_security_group_from_view_page(s_group_name_2, self.s_group_description)
-        s_group_id_2 = group2.get("s_group_id")
-        self.tester.add_ldap_rule_to_s_group(s_group_name_2, s_group_id_2)
-        self.tester.delete_security_group_from_view_page(s_group_name_2, s_group_id_2)
+        #s_group_name_1 = self.id_generator() + "_test_group"
+        #group1 = self.tester.create_security_group_from_dashboard(s_group_name_1, self.s_group_description)
+        #s_group_id_1 = group1.get("s_group_id")
+        #self.tester.add_tcp_22_rule_to_s_group(s_group_name_1, s_group_id_1)
+        #self.tester.add_custom_tcp_rule_to_s_group(s_group_name_1, s_group_id_1)
+        #self.tester.delete_security_group_from_detail_page(s_group_name_1, s_group_id_1)
+        #s_group_name_2 = self.id_generator() + "_test_group"
+        #group2 = self.tester.create_security_group_from_view_page(s_group_name_2, self.s_group_description)
+        #s_group_id_2 = group2.get("s_group_id")
+        #self.tester.add_ldap_rule_to_s_group(s_group_name_2, s_group_id_2)
+        #self.tester.delete_security_group_from_view_page(s_group_name_2, s_group_id_2)
         s_group_name_3 = self.id_generator() + "_test_group"
         group3 = self.tester.create_sesecurity_group_with_rules(s_group_name_3, self.s_group_description, "RDP", "Custom TCP", "22", "1024")
         s_group_id_3 = group3.get("s_group_id")
