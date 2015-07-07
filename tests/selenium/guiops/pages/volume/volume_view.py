0462c75e968911878c8514478ac4d72f22dda9eb
aliceh <alice@eucalyptus.com>
2015-07-07 14:56:18 -0700

    Adding attach and detach tests

--- tests/selenium/guiops/pages/volume/volume_view.py
+++ tests/selenium/guiops/pages/volume/volume_view.py
@@ -32,7 +32,7 @@ class VolumeView(ViewPage):
 
     def click_action_detach_volume_on_view_page(self, volume_id):
         self.tester.click_element_by_id(self._volume_action_menu_id.format(volume_id))
-        self.tester.click_element_by_css(self._delete_volume_actions_menu_item_css.format(volume_id))
+        self.tester.click_element_by_css(self._detach_volume_actions_menu_item_css.format(volume_id))
 
     def get_id_of_newly_created_volume(self, name=None):
         contains_id = self.tester.get_attribute_by_css(self._first_volume_link_in_list_css, "ng-href")
