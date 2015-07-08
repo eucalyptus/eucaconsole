3e42786d946722b2c2c71d9dbfec1b3a297fb924
dkavanagh <dkavanagh@gmail.com>
2015-07-07 21:30:08 -0400

    stop using hamburger, and updated some menuitem refs

--- tests/selenium/guiops/pages/basepage.py
+++ tests/selenium/guiops/pages/basepage.py
@@ -7,26 +7,27 @@ class BasePage(SeleniumApi):
     _user_dropdown_css = 'section#user-dropdown-section>a>span'
     _user_help_link='//ul[@id="user-dropdown"]/li/a'
     _user_logout_id="logout"
-    _dashboard_menuitem_id = "resource-menu-dashboard"
-    _images_menuitem_id = "resource-menu-images"
+    _dashboard_menuitem_id = "resource-menuitem-dashboard"
+    _images_menuitem_id = "resource-menuitem-images"
     _instances_menuitem_id = "resource-menuitem-instances"
-    _stacks_menuitem_id = ""
+    _instance_types_menuitem_id = "resource-menuitem-instance_types"
+    _stacks_menuitem_id = "resource-menuitem-stacks"
     _autoscaling_menuitem_id = "resource-menuitem-scalinggroups"
     _launchconfigs_menuitem_id =  "resource-menuitem-launchconfigs"
-    _storage_nav_bar_menu_id = "resource-menu-storage"
     _volumes_meniuitem_id = "resource-menuitem-volumes"
     _snapshot_menuitem_id = "resource-menuitem-snapshots"
     _buckets_menuitem_id = "esource-menuitem-buckets"
-    _network_sec_nav_bar_menu_id = "resource-menu-netsec"
     _sec_group_menuitem_id = "resource-menuitem-securitygroups"
     _keypair_menuitem_id = "resource-menuitem-keypairs"
-    _elastic_ips_menuitem_id = "resource-menuitem-eips"
+    _elastic_ips_menuitem_id = "resource-menuitem-ipaddresses"
+    _accounts_menuitem_id = "resource-menuitem-accounts"
     _users_menuitem_id = "resource-menuitem-users"
     _groups_menuitem_id = "resource-menuitem-groups"
     _roles_menuitem_id = "resource-menuitem-roles"
     _load_balancers_menuitem_id = "resource-menuitem-elb"
-    _iam_groups_menuitem_id = "resource-menuitem-users"
-    _iam_users_menuitem_id = "resource-menuitem-groups"
+    _iam_accounts_menuitem_id = "resource-menuitem-accounts"
+    _iam_users_menuitem_id = "resource-menuitem-users"
+    _iam_groups_menuitem_id = "resource-menuitem-groups"
     _iam_roles_menuitem_id = "resource-menuitem-roles"
     _notification_id="notifications"
     _page_title_id = "pagetitle"
@@ -39,67 +40,51 @@ class BasePage(SeleniumApi):
         self.tester.click_element_by_id(self._user_logout_id)
 
     def goto_dashboard_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._dashboard_menuitem_id)
 
     def goto_keypair_view_page_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id_robust(self._keypair_menuitem_id)
 
     def goto_images_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id_robust(self._images_menuitem_id)
 
     def goto_instances_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._instances_menuitem_id)
 
     def goto_stacks_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._stacks_menuitem_id)
 
     def goto_elestic_ip_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._elastic_ips_menuitem_id)
 
     def goto_security_groups_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._sec_group_menuitem_id)
 
     def goto_load_balancers_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._load_balancers_menuitem_id)
 
     def goto_volumes_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._volumes_meniuitem_id)
 
     def goto_snapshots_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._snapshot_menuitem_id)
 
     def goto_buckets_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._buckets_menuitem_id)
 
     def goto_asg_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._autoscaling_menuitem_id)
 
     def goto_launch_config_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._launchconfigs_menuitem_id)
 
     def goto_iam_users_view_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._iam_users_menuitem_id)
 
     def goto_iam_groups_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._iam_groups_menuitem_id)
 
     def goto_iam_roles_via_menu(self):
-        self.tester.click_element_by_css(self._hamburger_css)
         self.tester.click_element_by_id(self._iam_roles_menuitem_id)
 
     def get_notification(self):
