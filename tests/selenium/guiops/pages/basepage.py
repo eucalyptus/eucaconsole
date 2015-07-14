e58506b9d2eec8d44b2f3e42869273643048fe1e
aliceh <alice@eucalyptus.com>
2015-07-14 15:04:09 -0700

    Added scroll to goto aim users page method

--- tests/selenium/guiops/pages/basepage.py
+++ tests/selenium/guiops/pages/basepage.py
@@ -83,6 +83,7 @@ class BasePage(SeleniumApi):
         self.tester.click_element_by_id(self._launchconfigs_menuitem_id)
 
     def goto_iam_users_view_via_menu(self):
+        self.tester.scroll_to_element_by_id(self._iam_users_menuitem_id)
         self.tester.click_element_by_id(self._iam_users_menuitem_id)
 
     def goto_iam_groups_via_menu(self):
