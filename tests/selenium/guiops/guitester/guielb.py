60796a4b91621ac1248531d995b99cff8d03c80b
aliceh <alice@eucalyptus.com>
2015-07-20 10:55:42 -0700

    Added guielb

---
+++ tests/selenium/guiops/guitester/guielb.py
@@ -0,0 +1,8 @@
+from guitester import GuiTester
+from pages.basepage import BasePage
+from pages.dashboard import Dashboard
+
+class GuiELB(GuiTester):
+
+    def __init__(self, console_url, sauce=False, webdriver_url=None, browser=None, version=None, platform=None):
+        super(GuiELB, self).__init__(console_url, webdriver_url=webdriver_url, sauce=sauce, browser=browser, version=version, platform=platform)
