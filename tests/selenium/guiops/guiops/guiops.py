c40ca22b8bcfd654296407b51ff9a13c9e248122
aliceh <alice@eucalyptus.com>
2015-07-20 10:58:10 -0700

    Added GuiS3

--- tests/selenium/guiops/guiops/guiops.py
+++ tests/selenium/guiops/guiops/guiops.py
@@ -3,9 +3,10 @@ from guitester.guicf import GuiCF
 from guitester.guiiam import GuiIAM
 from guitester.guiasg import GuiASG
 from guitester.guielb import GuiELB
+from guitester.guis3 import GuiS3
 
 
-class GuiOps(GuiEC2, GuiCF, GuiIAM, GuiASG, GuiELB):
+class GuiOps(GuiEC2, GuiCF, GuiIAM, GuiASG, GuiS3):
     def __init__(self, console_url, sauce=False, webdriver_url=None, browser=None, version=None, platform=None):
         self.console_url = console_url
         self.sauce = sauce
