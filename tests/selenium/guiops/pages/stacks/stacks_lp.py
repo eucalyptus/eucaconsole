c5c1e92e6d14510be36d13576ea46858028230bb
aliceh <alice@eucalyptus.com>
2015-07-22 15:25:57 -0700

    Refactoring Stacks Landing

--- tests/selenium/guiops/pages/stacks/stacks_lp.py
+++ tests/selenium/guiops/pages/stacks/stacks_lp.py
@@ -6,7 +6,7 @@ class StacksLandingPage(LandingPage):
         self.tester = tester
         self.verify_stacks_view_page_loaded()
 
-    _stacks_view_page_title = "Stacks"
+    _stacks_lp_title = "Stacks"
     _create_stack_btn_id = "create-stack-btn"
     _stack_link_css = 'td>a[href="/stack/{0}"]'
     _stack_actions_menu_id = "table-item-dropdown_{0}"
@@ -17,8 +17,8 @@ class StacksLandingPage(LandingPage):
         """
         Waits for page title to load; waits for refresh button to load; wait for 'Create Stack' button to load.
         """
-        self.tester.wait_for_text_present_by_id(ViewPage._page_title_id, self._stacks_view_page_title)
-        self.tester.wait_for_visible_by_id(ViewPage._refresh_button_id)
+        self.tester.wait_for_text_present_by_id(LandingPage._page_title_id, self._stacks_lp_title)
+        self.tester.wait_for_visible_by_id(LandingPage._refresh_button_id)
         self.tester.wait_for_visible_by_id(self._create_stack_btn_id)
 
     def click_create_stack_button_on_view_page(self):
