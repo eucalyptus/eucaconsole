55654177f09fbdcdaecf92bc99d5fc945de008f4
aliceh <alice@eucalyptus.com>
2015-08-11 14:09:28 -0700

    Updated _volume_link_css after volume link was given an id

--- tests/selenium/guiops/pages/volume/volume_view.py
+++ tests/selenium/guiops/pages/volume/volume_view.py
@@ -15,7 +15,7 @@ class VolumeLanding(LandingPage):
     _manage_snapshots_actions_menu_item_css = "#item-dropdown_{0}>li:nth-of-type(2)>a"  #volume_id required
     _attach_to_instance_actions_menu_item_css = "#item-dropdown_{0}>li:nth-of-type(3)>a"  #volume_id required
     _first_volume_link_in_list_css = "#tableview>table>tbody>tr>td>a"
-    _volume_link_css = 'a[ng-href="/volumes/{0}"]'  #volume_id required;
+    _volume_link_css = "#table-id-column-{0}"  #volume_id required;
     _volume_status_xpath =  '//td/a[@href="/volumes/{0}"]/../../td[2]' #volume_id required;
     _search_input_field_css = ".search-input"
 
@@ -45,8 +45,7 @@ class VolumeLanding(LandingPage):
 
     def verify_volume_status_is_deleted(self, volume_id, timeout_in_seconds):
         # Note: Deleted volumes are filtered out from the landing page as of the 4.2 release of the console
-        volume_selector = '#table-id-column-{0}'.format(volume_id)
-        self.tester.wait_for_element_not_present_by_css(volume_selector)
+        self.tester.wait_for_element_not_present_by_css(self._volume_link_css.format(volume_id))
 
     def verify_volume_status_is_attached(self, volume_id, timeout_in_seconds):
         self.tester.wait_for_text_present_by_xpath(self._volume_status_xpath.format(volume_id), "attached", timeout_in_seconds)
