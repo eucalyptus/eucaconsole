b17c7816b81151b10cf8f7918bcd2e18a08fb4fc
dkavanagh <dkavanagh@gmail.com>
2015-09-03 14:56:50 -0400

    cleanup

--- tests/reqgenerator/reqgenerator.py
+++ tests/reqgenerator/reqgenerator.py
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup
 from cachecontrol import CacheControl
 
 # note, when using localhost and port, cache never filled.
-#URL = 'http://localhost:8888/'
 URL = 'http://dak-logging-elb-000562685431.lb.a-02.autoqa.qa1.eucalyptus-systems.com/'
 NUM_USERS = 50
 NUM_ITERATIONS = 100
@@ -49,7 +48,7 @@ class UserSession(object):
             self.session.get(self.url + l)
         end = datetime.now()
         page.elapsed = end - start
-        #print "cache size = "+str(len(self.session.adapters['http://'].cache.data))
+        # print "cache size = "+str(len(self.session.adapters['http://'].cache.data))
         return page
 
     def login(self, account, user, password):
@@ -63,7 +62,7 @@ class UserSession(object):
         page = self.get_page_completely(self.url)
         csrf_token = self.get_csrf_token_from_page(page)
         login_fields['csrf_token'] = csrf_token
-        p = self.session.post(self.url + 'login?login_type=Eucalyptus', data=login_fields)
+        self.session.post(self.url + 'login?login_type=Eucalyptus', data=login_fields)
         # TODO: return login status
 
     def logout(self):
@@ -85,6 +84,7 @@ class BrowsingUser(UserSession):
         'users',
         'stacks'
     ]
+
     def __call__(self):
         for i in range(self.iterations):
             page_name = self.pages[random.randrange(len(self.pages))]
@@ -92,9 +92,10 @@ class BrowsingUser(UserSession):
             load_time = page.elapsed.total_seconds()
             csrf_token = self.get_csrf_token_from_page(page)
             page = self.session.post(self.url + page_name + '/json', data={'csrf_token': csrf_token})
-            #load_time = load_time + page.elapsed.total_seconds()
+            load_time = load_time + page.elapsed.total_seconds()
             print "{0} loading page: {1} took {2} seconds".format(self.name, page_name, load_time)
             time.sleep(random.randrange(1, 20))
+
 
 class VolumeManipulatorUser(UserSession):
     def __call__(self):
@@ -108,7 +109,7 @@ class VolumeManipulatorUser(UserSession):
                 'zone': 'one'
             }
             create_fields['csrf_token'] = csrf_token
-            p = self.session.post(self.url + 'volumes/create', data=create_fields)
+            self.session.post(self.url + 'volumes/create', data=create_fields)
 
 if __name__ == "__main__":
     url = URL
@@ -126,7 +127,7 @@ if __name__ == "__main__":
     # start a bunch of users
     for i in range(0, num_users):
         s = requests.Session()
-        #s = CacheControl(s)
+        s = CacheControl(s)
         print "Starting user: " + str(i)
         u = BrowsingUser(url, s, 'user' + str(i), num_iterations)
         u.login('ui-test-acct-00', 'admin', 'mypassword0')
