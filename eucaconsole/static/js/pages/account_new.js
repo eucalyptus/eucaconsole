2251ce932a21853aad25156c53a38c6e1b4c9cc3
Adam A.G. Shamblin <adam.shamblin@hp.com>
2015-09-08 07:58:58 -0600

    GUI-1688 Fix jshint error.

--- eucaconsole/static/js/pages/account_new.js
+++ eucaconsole/static/js/pages/account_new.js
@@ -87,7 +87,7 @@ angular.module('CreateAccountPage', ['UserEditor', 'Quotas', 'EucaConsoleUtils']
                     }
 
                     return false;
-                }
+                };
             }
         };
     })
