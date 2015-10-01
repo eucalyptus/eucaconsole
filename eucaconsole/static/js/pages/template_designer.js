0a5bf4c1103d57079c5fb99f3f8b7a7a258a70e3
dkavanagh <dkavanagh@gmail.com>
2015-10-01 18:07:58 -0400

    GUI-2076 left toolbar now populates from list of resources

--- eucaconsole/static/js/pages/template_designer.js
+++ eucaconsole/static/js/pages/template_designer.js
@@ -1,7 +1,11 @@
 
-angular.module('TemplateDesigner', [])
-    .controller('TemplateDesignerCtrl', function($scope, $timeout) {
+angular.module('TemplateDesigner', ['EucaConsoleUtils'])
+    .controller('TemplateDesignerCtrl', function($scope, $timeout, eucaUnescapeJson) {
         var vm = this;
-        vm.initController = function() {
+        vm.initController = function(json_opts, blah) {
+            /* html escape */
+            json_opts = $('<div/>').html(json_opts).text();
+            var opts = JSON.parse(eucaUnescapeJson(json_opts));
+            vm.resources = opts.resources;
         };
     })
--- eucaconsole/views/templates.py
+++ eucaconsole/views/templates.py
@@ -1,4 +1,7 @@
 
+import logging
+import simplejson as json
+
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from pyramid.view import view_config
 
@@ -7,10 +10,30 @@ from ..views import BaseView, JSONResponse, JSONError
 class TemplateDesign(BaseView):
     TEMPLATE = '../templates/stacks/template_designer.pt'
 
+    RESOURCE_DEF = dict(
+        EC2=[
+            dict(name='Instance',
+            ),
+            dict(name='Volume',
+            )
+        ],
+        AutoScaling=[
+            dict(name='AutoScalingGroup',
+            ),
+            dict(name='LaunchConfiguration',
+            )
+        ]
+    )
+
     def __init__(self, request):
         super(TemplateDesign, self).__init__(request)
         self.render_dict = dict()
 
     @view_config(route_name='template_designer', renderer=TEMPLATE)
     def stack_view(self):
+        json_opts = dict(
+            resources=self.RESOURCE_DEF,
+        )
+        logging.info(BaseView.escape_json(json.dumps(json_opts)))
+        self.render_dict['json_opts'] = BaseView.escape_json(json.dumps(json_opts))
         return self.render_dict
