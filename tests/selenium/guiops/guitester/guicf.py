77c69739a81dd4a9a04ca6e051b0722fa274605a
dkavanagh <dkavanagh@gmail.com>
2015-10-07 19:11:09 -0400

    updates to stack create test

--- tests/selenium/guiops/guitester/guicf.py
+++ tests/selenium/guiops/guitester/guicf.py
@@ -1,8 +1,25 @@
 from guitester import GuiTester
 from pages.basepage import BasePage
+from pages.loginpage import LoginPage
 from pages.dashboard import Dashboard
+from pages.stacks.create_stack import CreateStackWizard
 
 class GuiCF(GuiTester):
 
     def __init__(self, console_url, sauce=False, webdriver_url=None, browser=None, version=None, platform=None):
         super(GuiCF, self).__init__(console_url, webdriver_url=webdriver_url, sauce=sauce, browser=browser, version=version, platform=platform)
+
+    def login(self, account, username, password):
+        """
+        Logs in to eucaconsole with credentials specified in the GuiTester object, verifies dashboard is loaded.
+        :param account:
+        :param username:
+        :param password:
+        """
+        LoginPage(self).login(account, username, password)
+        Dashboard(self).verify_dashboard_loaded()
+
+    def create_stack_from_dashboard(self, name):
+        BasePage(self).goto_dashboard_via_menu()
+        Dashboard(self).click_create_stack_link()
+        CreateStackWizard(self).create_stack(name)
--- tests/selenium/guiops/pages/stacks/create_stack.py
+++ tests/selenium/guiops/pages/stacks/create_stack.py
@@ -1,7 +1,14 @@
 from pages.detailpage import BasePage
 
 
-class CreateStackPage(BasePage):
+class CreateStackWizard(BasePage):
+    _create_stack_name_field_id = "name"
+    _sample_stack_selector_id = "sample-template"
 
     def __init__(self, tester):
         self.tester = tester
+
+    def create_stack(self, name):
+        self.tester.send_keys_by_id(self._create_stack_name_field_id, name)
+        import pdb; pdb.set_trace()
+        self.tester.select_by_id(self._sample_stack_selector_id, 'Basic Instance')
