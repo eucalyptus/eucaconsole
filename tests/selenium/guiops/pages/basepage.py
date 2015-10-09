a3e18609d9f6ad53a4325af2edc482bfb6246897
aliceh <alice@eucalyptus.com>
2015-10-09 13:14:56 -0700

    Changed all navigation methods to hit enter on sidebar nav menu item instead of click method.

--- tests/selenium/guiops/pages/basepage.py
+++ tests/selenium/guiops/pages/basepage.py
@@ -41,67 +41,67 @@ class BasePage(SeleniumApi):
 
     def goto_dashboard_via_menu(self):
         self.tester.scroll_to_element_by_id(self._dashboard_menuitem_id)
-        self.tester.click_element_by_id(self._dashboard_menuitem_id)
+        self.tester.send_keys_by_id(self._dashboard_menuitem_id, "\n", clear_field=False)
 
     def goto_keypair_view_page_via_menu(self):
         self.tester.scroll_to_element_by_id(self._keypair_menuitem_id)
-        self.tester.click_element_by_id(self._keypair_menuitem_id)
+        self.tester.send_keys_by_id(self._keypair_menuitem_id, "\n", clear_field=False)
 
     def goto_images_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._images_menuitem_id)
-        self.tester.click_element_by_id_robust(self._images_menuitem_id)
+        self.tester.send_keys_by_id(self._images_menuitem_id, "\n", clear_field=False)
 
     def goto_instances_via_menu(self):
         self.tester.scroll_to_element_by_id(self._instances_menuitem_id)
-        self.tester.click_element_by_id(self._instances_menuitem_id)
+        self.tester.send_keys_by_id(self._instances_menuitem_id, "\n", clear_field=False)
 
     def goto_stacks_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._stacks_menuitem_id)
-        self.tester.click_element_by_id(self._stacks_menuitem_id)
+        self.tester.send_keys_by_id(self._stacks_menuitem_id, "\n", clear_field=False)
 
     def goto_elestic_ip_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._elastic_ips_menuitem_id)
-        self.tester.click_element_by_id(self._elastic_ips_menuitem_id)
+        self.tester.send_keys_by_id(self._elastic_ips_menuitem_id, "\n", clear_field=False)
 
     def goto_security_groups_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._sec_group_menuitem_id)
-        self.tester.click_element_by_id(self._sec_group_menuitem_id)
+        self.tester.send_keys_by_id(self._sec_group_menuitem_id, "\n", clear_field=False)
 
     def goto_load_balancers_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._load_balancers_menuitem_id)
-        self.tester.click_element_by_id(self._load_balancers_menuitem_id)
+        self.tester.send_keys_by_id(self._load_balancers_menuitem_id, "\n", clear_field=False)
 
     def goto_volumes_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._volumes_meniuitem_id)
-        self.tester.click_element_by_id(self._volumes_meniuitem_id)
+        self.tester.send_keys_by_id(self._volumes_meniuitem_id, "\n", clear_field=False)
 
     def goto_snapshots_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._snapshot_menuitem_id)
-        self.tester.click_element_by_id_covert(self._snapshot_menuitem_id)
+        self.tester.send_keys_by_id(self._snapshot_menuitem_id, "\n", clear_field=False)
 
     def goto_buckets_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._buckets_menuitem_id)
-        self.tester.click_element_by_id(self._buckets_menuitem_id)
+        self.tester.send_keys_by_id(self._buckets_menuitem_id, "\n", clear_field=False)
 
     def goto_asg_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._autoscaling_menuitem_id)
-        self.tester.click_element_by_id(self._autoscaling_menuitem_id)
+        self.tester.send_keys_by_id(self._autoscaling_menuitem_id, "\n", clear_field=False)
 
     def goto_launch_config_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._launchconfigs_menuitem_id)
-        self.tester.click_element_by_id(self._launchconfigs_menuitem_id)
+        self.tester.send_keys_by_id(self._launchconfigs_menuitem_id, "\n", clear_field=False)
 
     def goto_iam_users_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._iam_users_menuitem_id)
-        self.tester.click_element_by_id(self._iam_users_menuitem_id)
+        self.tester.send_keys_by_id(self._iam_users_menuitem_id, "\n", clear_field=False)
 
     def goto_iam_groups_via_menu(self):
         self.tester.scroll_to_element_by_id(self._iam_groups_menuitem_id)
-        self.tester.click_element_by_id(self._iam_groups_menuitem_id)
+        self.tester.send_keys_by_id(self._iam_groups_menuitem_id, "\n", clear_field=False)
 
     def goto_iam_roles_via_menu(self):
         self.tester.scroll_to_element_by_id(self._iam_roles_menuitem_id)
-        self.tester.click_element_by_id(self._iam_roles_menuitem_id)
+        self.tester.send_keys_by_id(self._iam_roles_menuitem_id, "\n", clear_field=False)
 
     def get_notification(self):
         self.tester.wait_for_visible_by_id(BasePage._notification_id)
