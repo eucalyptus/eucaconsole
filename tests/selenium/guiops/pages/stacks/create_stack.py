8b0d16f1b24f9a3e0a79d7c52402098fc5936485
dkavanagh <dkavanagh@gmail.com>
2015-10-09 11:09:42 -0400

    GUI-2108 test to create stack. fixes to selenium_api, fixed chosen ids in stack wizard

--- tests/selenium/guiops/pages/stacks/create_stack.py
+++ tests/selenium/guiops/pages/stacks/create_stack.py
@@ -4,11 +4,26 @@ from pages.detailpage import BasePage
 class CreateStackWizard(BasePage):
     _create_stack_name_field_id = "name"
     _sample_stack_selector_id = "sample-template"
+    _next_step_button_id = "visit-step-2"
+    _param_keyname_selector_id = "KeyName"
+    _param_imageid_selector_id = "ImageId_chosen"
+    _param_imageid_search_field_css = "#ImageId_chosen>div.chosen-drop>div.chosen-search>input"
+    _create_from_snapshot_search_field_css = ".chosen-search>input"
+    _create_stack_button_id = "stack-wizard-btn-step2"
 
     def __init__(self, tester):
         self.tester = tester
 
     def create_stack(self, name):
+        # populate first step
         self.tester.send_keys_by_id(self._create_stack_name_field_id, name)
-        import pdb; pdb.set_trace()
         self.tester.select_by_id(self._sample_stack_selector_id, 'Basic Instance')
+        self.tester.wait_for_clickable_by_id(self._next_step_button_id)
+        self.tester.click_element_by_id(self._next_step_button_id)
+        # populate second step
+        self.tester.select_by_id(self._param_keyname_selector_id, index=1)
+
+        self.tester.click_element_by_id(self._param_imageid_selector_id)
+        self.tester.send_keys_by_css(self._param_imageid_search_field_css, 'precise\n')
+
+        self.tester.click_element_by_id(self._create_stack_button_id)
