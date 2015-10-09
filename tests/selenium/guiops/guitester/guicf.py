b1eb961c2bc68cc797934fd909454b6ca619ac69
dkavanagh <dkavanagh@gmail.com>
2015-10-09 11:52:30 -0400

    GUI-2109 added delete stack support

--- tests/selenium/guiops/guitester/guicf.py
+++ tests/selenium/guiops/guitester/guicf.py
@@ -2,7 +2,9 @@ from guitester import GuiTester
 from pages.basepage import BasePage
 from pages.loginpage import LoginPage
 from pages.dashboard import Dashboard
+from dialogs.stack_dialogs import DeleteStackDialog
 from pages.stacks.create_stack import CreateStackWizard
+from pages.stacks.stacks_lp import StacksLandingPage
 
 class GuiCF(GuiTester):
 
@@ -23,3 +25,9 @@ class GuiCF(GuiTester):
         BasePage(self).goto_dashboard_via_menu()
         Dashboard(self).click_create_stack_link()
         CreateStackWizard(self).create_stack(name)
+
+    def delete_stack_from_dashboard(self, name):
+        BasePage(self).goto_dashboard_via_menu()
+        BasePage(self).goto_stacks_view_via_menu()
+        StacksLandingPage(self).click_action_delete_stack_on_landing_page(name)
+        DeleteStackDialog(self).delete_stack()
