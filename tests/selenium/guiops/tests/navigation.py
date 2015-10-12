857e1ccb51ca1028a88aa6e10f47782d1f5b9b5c
aliceh <alice@eucalyptus.com>
2015-10-11 21:48:56 -0700

    Added  goto_images_lp method to navigation.py test

--- tests/selenium/guiops/tests/navigation.py
+++ tests/selenium/guiops/tests/navigation.py
@@ -1,10 +1,15 @@
-from guitester.guiec2 import GuiEC2
-from guitester.guiiam import GuiIAM
-import time
+from guiops.guiops import GuiOps
+from pages.basepage import BasePage
+from option_parser import Option_parser
+import string, random, time
+import logging, traceback
 
 
-class Navigation_sequence(GuiEC2):
+class Navigation_sequence(GuiOps):
 
+
+    def id_generator(self, size = 6, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
 
     def __init__(self):
         parser = Option_parser()
@@ -17,13 +22,14 @@ class Navigation_sequence(GuiEC2):
         self.browser = parser.parse_options()['browser']
         self.version = parser.parse_options()['version']
         self.platform = parser.parse_options()['platform']
-        self.tester = GuiEC2(console_url=self.console_url, webdriver_url=self.webdriver_url, sauce=self.sauce, browser=self.browser, version=self.version, platform=self.platform)
+        self.tester = GuiOps(console_url=self.console_url, webdriver_url=self.webdriver_url, sauce=self.sauce, browser=self.browser, version=self.version, platform=self.platform)
+        logging.basicConfig(format='%(asctime)s %(message)s')
 
 
     def navigation_test(self):
 
-        self.tester.login("ui-test-acct-00", "admin", "mypassword0")
-        self.tester.create_keypair_from_dashboard("keypair-test")
+        self.tester.login(self.account, self.user, self.password)
+        self.tester.goto_images_page_via_nav()
         self.tester.logout()
         self.tester.exit_browser()
 
