e05435ccf2fc089126602434328eba483995d337
aliceh <alice@eucalyptus.com>
2015-11-12 18:12:32 -0800

    Adding content to asg_lp

--- tests/selenium/guiops/pages/autoscaling/asg_lp.py
+++ tests/selenium/guiops/pages/autoscaling/asg_lp.py
@@ -18,6 +18,7 @@ class ASGLanding(LandingPage):
     _instances_xpath =_launch_config_link_xpath = 'xpath=//a[@ng-href="/scalinggroups/{0}"]/../../td[3]/a' #asg_name required
     _availability_zones_xpath = 'xpath=//a[@ng-href="/scalinggroups/{0}"]/../../td[4]' #asg_name required
     _capacity_xpath = 'xpath=//a[@ng-href="/scalinggroups/{0}"]/../../td[5]' #asg_name required
+    _status_xpath = 'xpath=//a[@ng-href="/scalinggroups/{0}"]/../../td[6]' #asg_name required
     _search_input_field_css = ".search-input"
 
     def verify_asg_lp_loaded(self):
