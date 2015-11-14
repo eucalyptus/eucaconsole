80ad40913bb3a0487a098ccbebe55ad6823d2daf
aliceh <alice@eucalyptus.com>
2015-11-13 17:19:56 -0800

    Adding content to launch config landing page

--- tests/selenium/guiops/pages/autoscaling/launch_config_detail.py
+++ tests/selenium/guiops/pages/autoscaling/launch_config_detail.py
@@ -27,6 +27,9 @@ class LaunchConfigDetailPage(DetailPage):
         self.tester.click_element_by_css(DetailPage._actions_menu_css)
         self.tester.click_element_by_id(self._create_lc_like_this_action_menuitem_id)
 
+    def create_asg_on_lc_detail_page(self):
+        self.tester.click_element_by_css(DetailPage._actions_menu_css)
+        self.tester.click_element_by_id(self._create_asg_action_menuitem_id)
 
 
 
