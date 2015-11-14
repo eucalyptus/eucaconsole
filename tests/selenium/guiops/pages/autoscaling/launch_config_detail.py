e7d55c84a422e86240a99d186bfbb04c9b5470fa
aliceh <alice@eucalyptus.com>
2015-11-13 17:10:42 -0800

    Adding content to launch config landing page

--- tests/selenium/guiops/pages/autoscaling/launch_config_detail.py
+++ tests/selenium/guiops/pages/autoscaling/launch_config_detail.py
@@ -8,7 +8,8 @@ class LaunchConfigDetailPage(DetailPage):
 
     _lc_detail_page_title = "Details for launch configuration: {0}" #lc name required
     _delete_lc_action_menuitem_id = "delete-launchconfig-action"
-
+    _create_lc_like_this_action_menuitem_id = "create-another-launchconfig-action"
+    _create_asg_action_menuitem_id = "create-scalinggroup-action"
 
 
     def verify_lc_detail_page_loaded(self, lc_name):
