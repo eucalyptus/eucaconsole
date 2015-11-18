8036aa215957185a987d52dc51b46d2e7966e115
aliceh <alice@eucalyptus.com>
2015-11-17 21:55:32 -0800

    Adding verifications guiasg methods

--- tests/selenium/guiops/dialogs/launch_config_dialogs.py
+++ tests/selenium/guiops/dialogs/launch_config_dialogs.py
@@ -7,6 +7,6 @@ class DeleteLaunchConfigModal(BasePage):
 
     _delete_lc_submit_button_id = "delete_launchconfig_submit_button"
 
-    def delete_asg(self):
+    def delete_launch_config(self):
         self.tester.click_element_by_id(self._delete_lc_submit_button_id)
 
