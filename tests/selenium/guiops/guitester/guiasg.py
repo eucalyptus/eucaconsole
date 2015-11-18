2237df1d2d1e9413182f45ea4b3721e8d872f62a
aliceh <alice@eucalyptus.com>
2015-11-17 21:19:14 -0800

    Adding verifications guiasg methods

--- tests/selenium/guiops/guitester/guiasg.py
+++ tests/selenium/guiops/guitester/guiasg.py
@@ -20,22 +20,29 @@ class GuiASG(GuiTester):
         CreateLaunchConfigPage(self).create_new_launch_config(lc_name, instance_type, image, key_name,
                                security_group, user_data_text, user_data_file_path, role, create_asg, kernel_id, ramdisk_id, enable_monitoring)
 
+
     def create_launch_config_and_asg_from_lc_lp(self, lc_name, asg_name):
         BasePage(self).goto_launch_config_view_via_menu()
         LaunchConfigLanding(self).click_create_lc_button_on_landing_page()
         CreateLaunchConfigPage(self).create_new_launch_config_and_asg(lc_name, asg_name)
+        BasePage(self).goto_asg_lp_via_menu()
+        ASGLanding(self).verify_asg_present(asg_name)
 
     def create_asg_from_dashboard(self, asg_name, launch_config_name, availabilityzones = None, min_cpapacity=None, desired_capacity=None, max_capacity=None, grace_period=None, loadbalancers=None):
         BasePage(self).goto_dashboard_via_menu()
         Dashboard(self).click_create_asg_link_from_dashboard()
         CreateASGPage(self).create_asg(asg_name, launch_config_name, availabilityzones, min_cpapacity, desired_capacity,
                                       max_capacity, grace_period, loadbalancers)
+        BasePage(self).goto_asg_lp_via_menu()
+        ASGLanding(self).verify_asg_present(asg_name)
 
     def create_asg_from_asg_lp(self, asg_name, launch_config_name, availabilityzones = None, min_cpapacity=None, desired_capacity=None, max_capacity=None, grace_period=None, loadbalancers=None):
         BasePage(self).goto_asg_lp_via_menu()
         ASGLanding(self).click_action_create_asg_on_landing_page()
         CreateASGPage(self).create_asg(asg_name, launch_config_name, availabilityzones, min_cpapacity, desired_capacity,
                                       max_capacity, grace_period, loadbalancers)
+        BasePage(self).goto_asg_lp_via_menu()
+        ASGLanding(self).verify_asg_present(asg_name)
 
     def delete_asg_from_lp(self, asg_name):
         BasePage(self).goto_asg_lp_via_menu()
