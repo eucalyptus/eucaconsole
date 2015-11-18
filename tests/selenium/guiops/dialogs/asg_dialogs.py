d400157adddda90d6e6e6b386d100ca817d2c374
aliceh <alice@eucalyptus.com>
2015-11-17 20:08:56 -0800

    Adding asg dialogs

---
+++ tests/selenium/guiops/dialogs/asg_dialogs.py
@@ -0,0 +1,15 @@
+from pages.basepage import BasePage
+
+class DeleteASGDialog(BasePage):
+
+    def __init__(self, tester):
+        self.tester = tester
+
+    _delete_associated_snaspshot_checkbox_id = "delete_snapshot"
+    _remove_image_from_cloud_submit_button_id = "deregister-image-button"
+
+    def delete_asg(self, delete_associated_snapshot=False):
+        if delete_associated_snapshot is True:
+            self.tester.click_element_by_id(self._delete_associated_snaspshot_checkbox_id)
+        self.tester.click_element_by_id(self._remove_image_from_cloud_submit_button_id)
+
