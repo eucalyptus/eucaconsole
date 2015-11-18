eb44135e8f74a6b865fc3415d9e1fe3e8a57bab9
aliceh <alice@eucalyptus.com>
2015-11-17 20:04:11 -0800

    Adding autoscaling  methods

--- tests/selenium/guiops/tests/autoscaling.py
+++ tests/selenium/guiops/tests/autoscaling.py
@@ -24,7 +24,7 @@ class ASG_operations_sequence(GuiOps):
 
     def asg_ops_test(self):
         self.tester.login(self.account, self.user, self.password)
-        self.tester.create_asg_from_dashboard("Alice-test-lc", "Alice-test-ASG")
+        self.tester.create_asg_from_dashboard(launch_config_name="Alice-test-lc", asg_name="Alice-test-ASG")
         self.tester.logout()
         self.tester.exit_browser()
 
