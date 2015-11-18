81cf40bf758695cb7841efcde7c31437f2d0200b
dkavanagh <dkavanagh@gmail.com>
2015-11-18 16:35:44 -0500

    GUI-1521 added test to ensure scaling history tab shows expected data

--- tests/selenium/guiops/pages/autoscaling/asg_detail.py
+++ tests/selenium/guiops/pages/autoscaling/asg_detail.py
@@ -10,6 +10,7 @@ class ASGDetailPage(DetailPage):
     _next_step_modal_id = "nextstep-scalinggroup-modal"
     _do_notshow_again_checkbox_id = "check-do-not-show-me-again"
     _close_modal_x_css = "#nextstep-scalinggroup-modal>a.close-reveal-modal"
+    _scaling_history_tab_css = "[href='/scalinggroups/{0}/history']" #asg name required
     _scaling_policies_tab_css = "[href='/scalinggroups/{0}/policies']" #asg name required
     _instances_tab_css = "[href='/scalinggroups/{0}/instances']" #asg name required
     _general_tab_css = '[href="/scalinggroups/{0}"]' #asg name required
@@ -22,6 +23,8 @@ class ASGDetailPage(DetailPage):
     _launch_config_dropdown_css = "#launch_config_chosen>a"
     _launch_config_search_field_css = "#launch_config_chosen>div>div>input"
     _launch_config_search_active_result_css = "li.active-result.result-selected"
+    _scaling_history_row_expando_css = "i.table-expando-closed:nth-of-type(1)"
+    _scaling_history_first_cause_css = "tbody:nth-of-type(1)>tr:nth-of-type(2)>td:nth-of-type(2)>div:nth-of-type(2)>div:nth-of-type(2)>div:nth-of-type(1)"
 
 
 
@@ -35,13 +38,26 @@ class ASGDetailPage(DetailPage):
         self.tester.wait_for_text_present_by_id(DetailPage(self)._detail_page_title_id, self._asg_detail_page_title.format(asg_name))
         self.tester.wait_for_element_present_by_css(DetailPage(self)._actions_menu_css)
 
+    def goto_scaling_history_tab(self, asg_name):
+        """
+        Checks if Scaling History tab is already open. Opens Scaling History tab if not.
+        """
+        tab = self.tester.store_text_by_css(self._active_tab_css)
+        print "Found active tab {0}".format(tab)
+        if tab in ['General', 'Instances', 'Scaling Policies']:
+            self.tester.click_element_by_css(self._scaling_history_tab_css.format(asg_name))
+        elif tab == "Scaling History":
+            pass
+        else:
+            print "ERROR: tab {0} not among recognized tab names.".format(tab)
+
     def goto_instances_tab(self, asg_name):
         """
         Checks if Instances tab is already open. Opens Instances tab if not.
         """
         tab = self.tester.store_text_by_css(self._active_tab_css)
         print "Found active tab {0}".format(tab)
-        if tab == "General" or tab == "Scaling Policies":
+        if tab in ['General', 'Scaling Policies', 'Scaling History']:
             self.tester.click_element_by_css(self._instances_tab_css.format(asg_name))
         elif tab == "Instances":
             pass
@@ -54,7 +70,7 @@ class ASGDetailPage(DetailPage):
         """
         tab = self.tester.store_text_by_css(self._active_tab_css)
         print "Found active tab {0}".format(tab)
-        if tab == "Instances" or tab == "Scaling Policies":
+        if tab in ['Instances', 'Scaling Policies', 'Scaling History']:
             self.tester.click_element_by_css(self._general_tab_css.format(asg_name))
         elif tab == "General":
             pass
@@ -67,7 +83,7 @@ class ASGDetailPage(DetailPage):
         """
         tab = self.tester.store_text_by_css(self._active_tab_css)
         print "Found active tab {0}".format(tab)
-        if tab == "General" or tab == "Instances":
+        if tab in ['General', 'Instances', 'Scaling History']:
             self.tester.click_element_by_css(self._scaling_policies_tab_css.format(asg_name))
         elif tab == "Scaling Policies":
             pass
@@ -94,10 +110,12 @@ class ASGDetailPage(DetailPage):
         self.tester.click_element_by_css(self._launch_config_search_active_result_css)
         self.tester.click_element_by_id(self._save_changes_button_id)
 
-
-
-
-
-
-
+    def verify_scaling_history(self, asg_name):
+        self.goto_scaling_history_tab(asg_name)
+        self.tester.click_element_by_css(self._scaling_history_row_expando_css)
+        text = self.tester.store_text_by_css(self._scaling_history_first_cause_css)
+        if text.find('an instance was started') > 0:
+            print 'Found expected cause {0}'.format(text)
+        else:
+            print 'ERROR: couldn\'t find expected scaling history cause text'
 
