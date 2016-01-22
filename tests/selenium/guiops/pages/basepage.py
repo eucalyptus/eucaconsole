7118cf6c6062e5478c581aa989865c73c48eff6f
aliceh <alice@eucalyptus.com>
2016-01-21 18:52:37 -0800

    Fix for navigation methods.

--- tests/selenium/guiops/pages/basepage.py
+++ tests/selenium/guiops/pages/basepage.py
@@ -67,14 +67,11 @@ class BasePage(SeleniumApi):
     def goto_keypair_view_page_via_menu(self):
         self.tester.scroll_to_element_by_id(self._keypair_menuitem_id)
         #self.tester.send_keys_by_id(self._keypair_menuitem_id, "\n", clear_field=False)
-        self.tester.click_element_by_id_robust(self._keypair_menuitem_id, self._refresh_button_from_lp_id)
+        self.tester.click_element_by_id_robust(self._keypair_menuitem_id, "create-keypair-btn")
 
     def goto_images_view_via_menu(self):
         self.tester.scroll_to_element_by_id(self._images_menuitem_id)
-        #self.tester.click_element_by_xpath(self._images_parent_xpath)
-        #self.tester.click_element_by_css(self._images_menuitem_css)
-        self.tester.click_element_by_id_robust(self._images_menuitem_id, self._refresh_button_from_lp_id)
-        #self.tester.send_keys_by_id(self._images_menuitem_id, "\n", clear_field=False)
+        self.tester.click_element_by_id_robust(self._images_menuitem_id, "li.current>a:contains('Images')")
 
     def goto_instances_via_menu(self):
         self.tester.scroll_to_element_by_id(self._instances_menuitem_id)
