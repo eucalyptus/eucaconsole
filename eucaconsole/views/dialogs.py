0037ebabafdd614d17a1bb1479b5e25d71b00023
Adam A.G. Shamblin <adam.shamblin@hpe.com>
2016-01-25 16:27:55 -0700

    GUI-2198, WIP

--- eucaconsole/views/dialogs.py
+++ eucaconsole/views/dialogs.py
@@ -180,6 +180,16 @@ def create_alarm_dialog(context, request, alarm_form=None, alarm_choices=None, r
     )
 
 
+@panel_config('delete_alarm_dialog', renderer='../templates/dialogs/delete_alarm_dialog.pt')
+def delete_alarm_dialog(context, request, modal_size='medium'):
+    '''
+    Delete alarm dialog page.
+    '''
+    return dict(
+        modal_size=modal_size
+    )
+
+
 @panel_config('keypair_dialogs', renderer='../templates/dialogs/keypair_dialogs.pt')
 def keypair_dialogs(context, request, keypair=None, landingpage=False, delete_form=None):
     """ Modal dialogs for Keypair landing and detail page."""
@@ -378,4 +388,3 @@ def cloudwatch_chart_dialog(context, request, duration_choices=None, statistic_c
 def ufshost_warn_dialog(context, request):
     """ Modal warning when trying to create a stack, but ufshost set to localhost"""
     return dict()
-
