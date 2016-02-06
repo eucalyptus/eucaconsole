7aa5c84cd99828f9f88139fda45a4be96aae75c9
Kamal Gill <kamal@kamalgill.net>
2016-02-05 17:25:57 -0800

    GUI-2430: More flexible handling of zones passed as an arbitrary length

--- tests/selenium/guiops/tests/volumes.py
+++ tests/selenium/guiops/tests/volumes.py
@@ -16,18 +16,19 @@ class VolumeOperationsSequence(GuiOps):
         self.browser = options['browser']
         self.version = options['version']
         self.platform = options['platform']
-        self.zones = [zone.strip() for zone in options['zones'].split(',')]
-        self.zone_one = self.zones[0]
-        self.zone_two = self.zones[1] if len(self.zones) > 1 else self.zone_one
+        # Get dict of zones keyed by index (e.g {0: 'one', 1: 'two'})
+        self.zones = dict(enumerate(options['zones'].split(',')))
         self.tester = GuiOps(console_url=self.console_url, webdriver_url=self.webdriver_url,
                              sauce=self.sauce, browser=self.browser, version=self.version, platform=self.platform)
 
     def volume_ops_test(self):
         self.tester.login(self.account, self.user, self.password)
         volume1_name = self.id_generator()+"-volume"
-        volume1 = self.tester.create_volume_from_view_page(volume1_name, volume_size=2, availability_zone=self.zone_one)
+        volume1 = self.tester.create_volume_from_view_page(volume1_name, volume_size=2,
+                                                           availability_zone=self.zones.get(0))
         volume1_id = volume1.get("volume_id")
-        instance1 = self.tester.launch_instance_from_dashboard(availability_zone=self.zone_one, timeout_in_seconds=400)
+        instance1 = self.tester.launch_instance_from_dashboard(availability_zone=self.zones.get(0),
+                                                               timeout_in_seconds=400)
         instance1_id = instance1.get("instance_id")
         self.tester.verify_attach_notice_on_volume_monitoring_page(volume1_id)
         self.tester.attach_volume_from_instance_detail_page(volume1_id, instance1_id, timeout_in_seconds=400)
@@ -41,7 +42,8 @@ class VolumeOperationsSequence(GuiOps):
         self.tester.attach_volume_from_volume_lp(instance1_id, volume1_id, timeout_in_seconds=800)
         self.tester.terminate_instance_from_view_page(instance1_id)
         self.tester.delete_volume_from_view_page(volume1_id)
-        volume2 = self.tester.create_volume_from_dashboard(volume_size=3, availability_zone=self.zone_two)
+        volume2 = self.tester.create_volume_from_dashboard(
+            volume_size=3, availability_zone=self.zones.get(1, self.zones.get(0)))
         volume2_id = volume2.get("volume_id")
         self.tester.delete_volume_from_detail_page(volume2_id)
         self.sortable_volumes_tables_test()
@@ -50,10 +52,12 @@ class VolumeOperationsSequence(GuiOps):
 
     def sortable_volumes_tables_test(self):
         volume1_name = "!!!!"+self.id_generator()+"-volume"
-        volume1 = self.tester.create_volume_from_view_page(volume1_name, volume_size=1, availability_zone=self.zone_one)
+        volume1 = self.tester.create_volume_from_view_page(
+            volume1_name, volume_size=1, availability_zone=self.zones.get(0))
         volume1_id = volume1.get("volume_id")
         volume2_name = "~~~~"+self.id_generator()+"-volume"
-        volume2 = self.tester.create_volume_from_dashboard(volume2_name, volume_size=1, availability_zone=self.zone_one)
+        volume2 = self.tester.create_volume_from_dashboard(
+            volume2_name, volume_size=1, availability_zone=self.zones.get(0))
         volume2_id = volume2.get("volume_id")
         # Test ascending sort by name column
         self.tester.click_sortable_column_header_on_volumes_landing_page(column_name='name')
