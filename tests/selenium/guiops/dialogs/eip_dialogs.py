0a145dfe39ebea7035143883fc807e2e87000681
aliceh <alice@eucalyptus.com>
2016-02-18 20:18:48 -0800

    Modified wait in allocate eip method

--- tests/selenium/guiops/dialogs/eip_dialogs.py
+++ tests/selenium/guiops/dialogs/eip_dialogs.py
@@ -1,4 +1,5 @@
 from pages.basepage import BasePage
+import time
 
 
 class AllocateEipDialog(BasePage):
@@ -13,7 +14,8 @@ class AllocateEipDialog(BasePage):
         :return: allocated IPs as a sorted list of strings
         """
         self.tester.send_keys_by_css(self._allocate_elastic_ips_number_input_css, str(number))
-        self.tester.wait_for_enabled_by_id(self._allocate_elastic_ips_button_id)
+        self.tester.wait_for_clickable_by_id(self._allocate_elastic_ips_button_id)
+        #time.sleep(1)
         self.tester.click_element_by_id(self._allocate_elastic_ips_button_id)
         notification = self.tester.store_text_by_css(self._notification_css)
         notification = notification.replace('Successfully allocated IPs', '').strip()
