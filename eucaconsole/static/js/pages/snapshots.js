f0f4b0638e8e5a68e229fc595803625439367278
Kamal Gill <kamal@kamalgill.net>
2016-02-18 22:14:57 -0800

    GUI-2306: Refactor building images list when deleting snapshots that have one or more registered images

--- eucaconsole/static/js/pages/snapshots.js
+++ eucaconsole/static/js/pages/snapshots.js
@@ -10,7 +10,8 @@ angular.module('SnapshotsPage', ['LandingPage', 'EucaConsoleUtils'])
         $scope.snapshotID = '';
         $scope.snapshotName = '';
         $scope.imagesURL = '';
-        $scope.images = undefined;
+        $scope.images = [];
+        $scope.multipleItemsSelected = false;
         $scope.initSnapshots = function (imagesURL) {
             $scope.imagesURL = imagesURL;
         };
@@ -18,10 +19,30 @@ angular.module('SnapshotsPage', ['LandingPage', 'EucaConsoleUtils'])
             var modal = $('#' + action + '-snapshot-modal');
             $scope.snapshotID = snapshot.id;
             $scope.snapshotName = snapshot.name;
-            if (action == "delete") {
-                $scope.images = undefined;
+            if (action === "delete") {
+                $scope.images = [];
                 $scope.getSnapshotImages(snapshot, $scope.imagesURL);
             }
+            modal.foundation('reveal', 'open');
+        };
+        $scope.revealMultiSelectModal = function (action, selectedItems) {
+            var modal = $('#' + action + '-snapshot-modal'),
+                snapshotIDs = [],
+                snapshotNames = [];
+            selectedItems.forEach(function (item) {
+                snapshotIDs.push(item.id);
+                snapshotNames.push(item.name || item.id);
+            });
+            if (action === "delete") {
+                $scope.images = [];
+                selectedItems.forEach(function (snapshot) {
+                    var snapshotImages = $scope.getSnapshotImages(snapshot, $scope.imagesURL);
+                    $scope.images.push.apply($scope.images, snapshotImages);
+                });
+            }
+            $scope.multipleItemsSelected = snapshotIDs.length > 1;
+            $scope.snapshotID = snapshotIDs.join(', ');
+            $scope.snapshotName = snapshotNames.join(', ');
             modal.foundation('reveal', 'open');
         };
         $scope.getSnapshotImages = function (snapshot, url) {
@@ -29,7 +50,7 @@ angular.module('SnapshotsPage', ['LandingPage', 'EucaConsoleUtils'])
             $http.get(url).success(function(oData) {
                 var results = oData ? oData.results : '';
                 if (results && results.length > 0) {
-                    $scope.images = results;
+                    return results;
                 }
             }).error(function (oData, status) {
                 eucaHandleError(oData, status);
