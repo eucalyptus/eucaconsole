e8bf3e8b27b2412f8bc00a4960170e02138718db
Kamal Gill <kamal@kamalgill.net>
2016-02-26 16:24:54 -0800

    GUI-2307: Enable multi-select actions on security groups landing page

--- eucaconsole/static/js/pages/securitygroups.js
+++ eucaconsole/static/js/pages/securitygroups.js
@@ -8,10 +8,24 @@
 angular.module('SecurityGroupsPage', ['LandingPage'])
     .controller('SecurityGroupsCtrl', function ($scope) {
         $scope.securitygroupID = '';
+        $scope.multipleItemsSelected = false;
         $scope.revealModal = function (action, securitygroup) {
             var modal = $('#' + action + '-securitygroup-modal');
             $scope.securitygroupID = securitygroup.id;
             $scope.securitygroupName = securitygroup.name;
+            modal.foundation('reveal', 'open');
+        };
+        $scope.revealMultiSelectModal = function (action, selectedItems) {
+            var modal = $('#' + action + '-securitygroup-modal'),
+                itemIDs = [],
+                itemNames = [];
+            selectedItems.forEach(function (item) {
+                itemIDs.push(item.id);
+                itemNames.push(item.name || item.id);
+            });
+            $scope.multipleItemsSelected = itemIDs.length > 1;
+            $scope.securitygroupID = itemIDs.join(', ');
+            $scope.securitygroupName = itemNames.join(', ');
             modal.foundation('reveal', 'open');
         };
     })
