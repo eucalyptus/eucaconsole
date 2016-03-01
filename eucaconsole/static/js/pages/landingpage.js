89849c57f05b7914dfed39d4a6b04c0020dfa303
Kamal Gill <kamal@kamalgill.net>
2016-02-29 19:48:50 -0800

    GUI-2446: Avoid wiping out selected items and automatically closing More Actions menu during smart refresh

--- eucaconsole/static/js/pages/landingpage.js
+++ eucaconsole/static/js/pages/landingpage.js
@@ -14,6 +14,7 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize', 'MagicSearch', 'Ex
         $scope.runningSmartRefresh = false;
         $scope.facetItems = [];
         $scope.unfilteredItems = [];
+        $scope.selectedItems = [];
         $scope.filterKeys = [];
         $scope.sortBy = '';
         $scope.landingPageView = "tableview";
@@ -139,6 +140,29 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize', 'MagicSearch', 'Ex
                     regionKey, $('#region-dropdown').children('li[data-selected="True"]').children('a').attr('id'));
             }
         };
+        $scope.restoreSelectedItemsState = function () {
+            // Avoid wiping out selected items and automatically closing More Actions menu during smart refresh
+            // NOTE: Do not wrap this in a $scope.runningSmartRefresh check since that would prevent
+            //       the selected items from being restored after the final smart refresh cycle
+            var moreActionsBtn = $('#more-actions-btn');
+            var dropdownOpen = moreActionsBtn.hasClass('open');
+            var selectedItemIds = $scope.selectedItems.map(function (item) {
+                return item.id || item.name;
+            });
+            $timeout(function () {
+                angular.forEach($scope.items, function(item) {
+                    var itemId = item.id || item.name;
+                    if (selectedItemIds.indexOf(itemId) !== -1) {
+                        item.selected = true;
+                    }
+                });
+                if (dropdownOpen) {
+                    $timeout(function () {
+                        moreActionsBtn.click();
+                    });
+                }
+            });
+        };
         $scope.getItems = function (okToRefresh) {
             var csrf_token = $('#csrf_token').val();
             var data = "csrf_token="+csrf_token;
@@ -169,6 +193,7 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize', 'MagicSearch', 'Ex
                     $scope.$emit('itemsLoaded', $scope.items);
                     // and re-open any action menus
                     $scope.clickOpenDropdown();
+                    $scope.restoreSelectedItemsState();
                     $(document).foundation('tab', 'reflow');
                 });
                 if ($scope.serverFilter === false) {
@@ -353,12 +378,18 @@ angular.module('LandingPage', ['CustomFilters', 'ngSanitize', 'MagicSearch', 'Ex
             if (!$scope.state.allSelected && checkedIems.length === $scope.items.length) {
                 $scope.state.allSelected = true;
             }
+            $scope.selectedItems = checkedIems;
         };
         $scope.setAllState = function() {
             $timeout(function() {
                 angular.forEach($scope.items, function(item) {
                     item.selected = $scope.state.allSelected;
                 });
+                if ($scope.state.allSelected) {
+                    $scope.selectedItems = $scope.items;
+                } else {
+                    $scope.selectedItems = [];
+                }
             });
         };
         $scope.$on('searchUpdated', function($event, query) {
