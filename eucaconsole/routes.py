d5ef3030254c54c4057c62486b811d9bc6dca9f7
dkavanagh <dkavanagh@gmail.com>
2016-03-07 16:26:17 -0500

    removed old tag_editor_view

--- eucaconsole/routes.py
+++ eucaconsole/routes.py
@@ -378,8 +378,4 @@ urls = [
     Route(name='stack_state_json', pattern='/stacks/{name}/state/json'),
     Route(name='stack_template', pattern='/stacks/{name}/template'),
     Route(name='stack_events', pattern='/stacks/{name}/events'),
-
-    # Tag Editor #####
-    # Directive Template
-    Route(name='tag_editor_template', pattern='/templates/tag-editor'),
 ]
