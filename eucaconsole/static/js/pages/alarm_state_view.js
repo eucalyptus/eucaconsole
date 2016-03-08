cb136cccf37906a62f08aec81b3471658603f973
dkavanagh <dkavanagh@gmail.com>
2016-03-08 14:18:44 -0500

    GUI-2467 more working

--- eucaconsole/static/js/pages/alarm_state_view.js
+++ eucaconsole/static/js/pages/alarm_state_view.js
@@ -1,18 +1,31 @@
-angular.module('AlarmStateView', ['EucaConsoleUtils'])
-    .directive('alarmStateView', function(eucaUnescapeJson) {
+angular.module('AlarmStateView', ['EucaRoutes', 'EucaConsoleUtils'])
+    .directive('alarmStateView', function() {
         return {
             scope: {
-                template: '@'
+                template: '@',
+                resourceId: '='
             },
             restrict: 'E',
             templateUrl: function (element, attributes) {
                 return attributes.template;
             },
-            controller: ['$scope', '$http', 'eucaHandleError', function($scope, $http, eucaRoutes, eucaHandleError) {
+            controller: ['$scope', '$http', 'eucaRoutes', 'eucaHandleError', function($scope, $http, eucaRoutes, eucaHandleError) {
                 $scope.alarms = [];
                 $scope.toggleContent = function() {
                     $scope.expanded = !$scope.expanded;
                 };
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                var csrf_token = $('input[name="csrf_token"]').val();
+                var data = "&csrf_token=" + csrf_token;
+                $http({method:'POST', url: eucaRoutes.getRoute('alarms_for_resource', {'id': $scope.resourceId}),
+                    data:data,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(function successCallback(oData) {
+                    var response = oData.data ? oData.data : [];
+                    $scope.alarms = response.alarms;
+                }, function errorCallback(errData) {
+                    eucaHandleError(errData.data.message, errData.status);
+                });
             }]
         };
     })
