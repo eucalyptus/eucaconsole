f75bb3cb87fc0191676d659b72d1148f576fed76
Kamal Gill <kamal@kamalgill.net>
2016-03-10 11:58:11 -0800

    GUI-2467: Fix template lookup path in common render_template view

--- eucaconsole/views/directives.py
+++ eucaconsole/views/directives.py
@@ -1,7 +1,8 @@
 from pyramid.view import view_config
 from pyramid.renderers import render_to_response
 
+
 @view_config(route_name='render_template')
 def render_template(request):
     template_name = '/'.join(request.subpath)
-    return render_to_response('eucaconsole.templates:{0}.pt'.format(template_name), {}, request=request)
+    return render_to_response('eucaconsole:templates/{0}.pt'.format(template_name), {}, request=request)
