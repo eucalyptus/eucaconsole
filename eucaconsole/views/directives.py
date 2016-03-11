d7815b150a6a719e0241c9e9c0ca8c4a83514ce7
dkavanagh <dkavanagh@gmail.com>
2016-03-10 21:02:59 -0500

    Reverting previous commit

--- eucaconsole/views/directives.py
+++ eucaconsole/views/directives.py
@@ -5,4 +5,4 @@ from pyramid.renderers import render_to_response
 @view_config(route_name='render_template')
 def render_template(request):
     template_name = '/'.join(request.subpath)
-    return render_to_response('eucaconsole.templates:{0}.pt'.format(template_name), {}, request=request)
+    return render_to_response('eucaconsole:templates/{0}.pt'.format(template_name), {}, request=request)
