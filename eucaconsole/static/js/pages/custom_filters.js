68c909242ca34671d839546afb9770ea2d58f56b
Kamal Gill <kamal@kamalgill.net>
2016-03-16 11:09:12 -0700

    GUI-2463: Simplify space-to-underscore replacement in toClassName filter

--- eucaconsole/static/js/pages/custom_filters.js
+++ eucaconsole/static/js/pages/custom_filters.js
@@ -53,7 +53,7 @@ angular.module('CustomFilters', [])
 })
 .filter('toClassName', function () {
     return function (input) {
-        return input.replace(/\s+/g, '_').toLowerCase();
+        return input.replace(/ /g, '_').toLowerCase();
     };
 })
 ;
