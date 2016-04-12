069795ebf59434499300c6fd95f2d46c9886d10d
dkavanagh <dkavanagh@gmail.com>
2016-04-12 10:33:19 -0400

    forgot to save this file

--- eucaconsole/static/js/pages/metricgraph.js
+++ eucaconsole/static/js/pages/metricgraph.js
@@ -6,13 +6,13 @@ angular.module('MetricGraphPage', ['CloudWatchCharts'])
         if (params.graph !== undefined) {
             // parse graph params
             $scope.graph = purl("?"+$.base64.decode(params.graph)).param();
-            graph.dimensions = JSON.parse(graph.dimensions);
-            if (graph.stat !== undefined) {
-                if (graph.duration !== undefined) {
-                    $scope.$broadcast("cloudwatch:updateLargeGraphParams", graph.stat, parseInt(graph.period), parseInt(graph.duration));
+            $scope.graph.dimensions = JSON.parse($scope.graph.dimensions);
+            if ($scope.graph.stat !== undefined) {
+                if ($scope.graph.duration !== undefined) {
+                    $scope.$broadcast("cloudwatch:updateLargeGraphParams", $scope.graph.stat, parseInt($scope.graph.period), parseInt($scope.graph.duration));
                 }
                 else {
-                    $scope.$broadcast("cloudwatch:updateLargeGraphParams", graph.stat, parseInt(graph.period), undefined, new Date(graph.startTime), new Date(graph.endTime));
+                    $scope.$broadcast("cloudwatch:updateLargeGraphParams", $scope.graph.stat, parseInt($scope.graph.period), undefined, new Date($scope.graph.startTime), new Date($scope.graph.endTime));
                 }
             }
         }
