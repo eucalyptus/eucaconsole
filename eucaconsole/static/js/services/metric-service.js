260637f667409c08971dc26a4d566f062a2f85cd
Adam A.G. Shamblin <adam.shamblin@hpe.com>
2016-04-13 11:19:35 -0600

    GUI-2457, dimensions validation, metrics promise issue.

--- eucaconsole/static/js/services/metric-service.js
+++ eucaconsole/static/js/services/metric-service.js
@@ -1,27 +1,21 @@
 angular.module('MetricServiceModule', ['EucaRoutes'])
 .factory('MetricService', ['$http', 'eucaRoutes', function ($http, eucaRoutes) {
-    var _metrics = {};
-
     return {
         getMetrics: function (namespace, type, value) {
-            if(value in _metrics) {
-                return _metrics[value];
-            }
             return eucaRoutes.getRouteDeferred('metrics_available_for_resource', {type: type, value: value})
                 .then(function (path) {
                     return $http({
                         method: 'GET',
                         url: path
                     }).then(function (result) {
-                        var metrics;
+                        var metrics = [];
                         if(result && result.data) {
                             metrics = result.data.metrics || [];
                             metrics = metrics.filter(function (current ) {
                                 return current.namespace == namespace;
                             });
-                            _metrics[value] = metrics;
                         }
-                        return _metrics[value];
+                        return metrics;
                     });
                 });
         }
