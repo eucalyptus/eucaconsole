bf6a5f5674b9eb96398dcdbbefe02fa89a63f516
Kamal Gill <kamal@kamalgill.net>
2016-04-14 17:22:49 -0700

    GUI-2409: Leverage Angular form validation for Create Scaling Policy form

--- eucaconsole/static/js/pages/scalinggroup_policy.js
+++ eucaconsole/static/js/pages/scalinggroup_policy.js
@@ -16,55 +16,14 @@ angular.module('ScalingGroupPolicy', ['EucaConsoleUtils', 'CloudWatchCharts', 'C
         $scope.isNotValid = true;
         $scope.initController = function (alarmChoices) {
             $rootScope.alarmChoices = alarmChoices;
-            $scope.setWatch();
         };
-        $scope.checkRequiredInput = function () {
-            $scope.isNotValid = false;
-            if( $scope.policyName === '' || $scope.policyName === undefined ){
-                $scope.isNotValid = true;
-            }else if( $scope.adjustmentAmount === '' || $scope.adjustmentAmount === undefined ){
-                $scope.isNotValid = true;
-            }else if( $scope.coolDown === '' || $scope.coolDown === undefined ){
-                $scope.isNotValid = true;
-            }else if( $rootScope.alarm === '' || $rootScope.alarm === undefined || 
-                $rootScope.alarm === null || $rootScope.alarm === '""' ){
-                $scope.isNotValid = true;
-            }
-        };
-        $scope.setWatch = function () {
-            $scope.$watch('policyName', function () {
-                $scope.checkRequiredInput();
-            });
-            $scope.$watch('adjustmentAmount', function (newVal) {
-                if(newVal) {
-                    $scope.adjustmentAmount = eucaNumbersOnly(newVal);
-                    $scope.isNotValid = false;
-                } else {
-                    $scope.isNotValid = true;
-                }
-                $scope.checkRequiredInput();
-            });
-            $scope.$watch('coolDown', function (newVal) {
-                if(newVal) {
-                    $scope.coolDown = eucaNumbersOnly(newVal);
-                    $scope.isNotValid = false;
-                } else {
-                    $scope.isNotValid = true;
-                }
-                $scope.checkRequiredInput();
-            });
-            $scope.$watch('alarm', function () {
-                $scope.checkRequiredInput();
-            });
-
-            $scope.$on('alarmStateView:refreshList', function ($event, args) {
-                // Add new alarm to choices and set it as selected
-                var newAlarm = args.name;
-                $rootScope.alarmChoices[newAlarm] = newAlarm;
-                $rootScope.alarm = newAlarm;
-                $scope.isCreatingAlarm = false;
-            });
-        };
+        $scope.$on('alarmStateView:refreshList', function ($event, args) {
+            // Add new alarm to choices and set it as selected
+            var newAlarm = args.name;
+            $rootScope.alarmChoices[newAlarm] = newAlarm;
+            $rootScope.alarm = newAlarm;
+            $scope.isCreatingAlarm = false;
+        });
     })
 ;
 
