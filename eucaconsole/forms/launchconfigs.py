55cdc62b3ff6dd94876f09ed09e9b126407aef2c
Kamal Gill <kamal@kamalgill.net>
2016-05-09 13:04:53 -0700

    GUI-2378: Remove errant condition for kernel/ramdisk choices in launch config wizard

--- eucaconsole/forms/launchconfigs.py
+++ eucaconsole/forms/launchconfigs.py
@@ -132,9 +132,8 @@ class CreateLaunchConfigForm(BaseSecureForm):
         self.securitygroup.choices = self.choices_manager.security_groups(
             securitygroups=self.securitygroups, use_id=True, add_blank=False)
         self.role.choices = ChoicesManager(self.iam_conn).roles(add_blank=True)
-        if self.image is not None:
-            self.kernel_id.choices = self.choices_manager.kernels(image=self.image)
-            self.ramdisk_id.choices = self.choices_manager.ramdisks(image=self.image)
+        self.kernel_id.choices = self.choices_manager.kernels(image=self.image)
+        self.ramdisk_id.choices = self.choices_manager.ramdisks(image=self.image)
         self.associate_public_ip_address.choices = self.get_associate_public_ip_address_choices()
 
         # Set init data for security group
