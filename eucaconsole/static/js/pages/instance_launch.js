f9ea3c0d1091b27dedf455127b5013333f23828b
dkavanagh <dkavanagh@gmail.com>
2016-06-30 20:56:59 -0400

    more tweaks

--- eucaconsole/static/js/pages/instance_launch.js
+++ eucaconsole/static/js/pages/instance_launch.js
@@ -7,7 +7,7 @@
  */
 
 // Launch Instance page includes the Tag Editor, the Image Picker, BDM editor, and security group rules editor
-angular.module('LaunchInstance', ['TagEditor', 'BlockDeviceMappingEditor', 'ImagePicker', 'SecurityGroupRules', 'EucaConsoleUtils', 'ui.select'])
+angular.module('LaunchInstance', ['TagEditor', 'BlockDeviceMappingEditor', 'ImagePicker', 'SecurityGroupRules', 'EucaConsoleUtils', 'ngSanitize', 'ui.select'])
     .controller('LaunchInstanceCtrl', function ($scope, $http, $timeout, eucaHandleError, eucaUnescapeJson) {
         $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         $scope.launchForm = $('#launch-instance-form');
