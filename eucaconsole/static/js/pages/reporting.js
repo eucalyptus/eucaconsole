ea564d8270d15287639d2d228ffddc7f15cb709b
dkavanagh <dkavanagh@gmail.com>
2016-10-18 22:26:19 -0400

    GUI-2794 initial reporting page w/ tabs

---
+++ eucaconsole/static/js/pages/reporting.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * @fileOverview ElasticIP Detail Page JS
+ * @requires AngularJS
+ *
+ */
+
+angular.module('ReportingPage', [])
+    .controller('ReportingPageCtrl', function ($scope) {
+    })
+;
+
+
+
