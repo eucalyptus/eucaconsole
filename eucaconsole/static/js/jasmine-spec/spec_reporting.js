7a62cbb121ca823b07a2a2b703d15fab5d190739
dkavanagh <dkavanagh@gmail.com>
2016-10-20 17:02:07 -0400

    GUI-2794 added test cases for functions in navigation controller

--- eucaconsole/static/js/jasmine-spec/spec_reporting.js
+++ eucaconsole/static/js/jasmine-spec/spec_reporting.js
@@ -8,25 +8,53 @@
 
 describe("Reporting", function() {
 
-    beforeEach(angular.mock.module('Reporting'));
+    beforeEach(angular.mock.module('ReportingPage'));
 
-    var $scope, $location;
-    beforeEach(angular.mock.inject(function(_$location_, _$rootScaope_) {
+    var $rootScope, $location, $compile;
+    beforeEach(angular.mock.inject(function(_$location_, _$rootScope_, _$compile_) {
+        $rootScope = _$rootScope_;
         $location = _$location_;
-        $scope = _$rootScope_.$new();
+        $compile = _$compile_;
     }));
 
+    var controller;
     beforeEach(function() {
-        var template = window.__html__['templates/reporting/reporting.pt'];
-        setFixtures(template);
+        var element = $compile(
+            '<dl navigation="" reporting-configured="true"><dd></dd><dd></dd></dl>'
+        )($rootScope);
+        $rootScope.$digest();
+
+        $location.path('/reporting/dashboard');
+        $rootScope.$apply();
+
+        controller = element.controller('navigation');
     });
 
     describe("initial tab test", function() {
         it("should set preferences path", function() {
-            $location.path('/reporting/dashboard');
-            expect(location.path() == '/reporting/preferences').toBe(false);
-            scope.setInitialTab('false');
-            expect(location.path() == '/reporting/preferences').toBe(true);
+            expect($location.path() == '/reporting/preferences').toBe(false);
+            controller.setInitialTab('false');
+            expect($location.path() == '/reporting/preferences').toBe(true);
+        });
+    });
+
+    describe("active tab test", function() {
+        it("should set active tab state", function() {
+            expect(controller.isTabActive('/dashboard') == 'active').toBe(true);
+            expect(controller.isTabActive('/reports') == 'active').toBe(false);
+            expect(controller.isTabActive('/preferences') == 'active').toBe(false);
+            $location.path('/reporting/reports');
+            expect(controller.isTabActive('/dashboard') == 'active').toBe(false);
+            expect(controller.isTabActive('/reports') == 'active').toBe(true);
+            expect(controller.isTabActive('/preferences') == 'active').toBe(false);
+            $location.path('/reporting/preferences');
+            expect(controller.isTabActive('/dashboard') == 'active').toBe(false);
+            expect(controller.isTabActive('/reports') == 'active').toBe(false);
+            expect(controller.isTabActive('/preferences') == 'active').toBe(true);
+            $location.path('/reporting/blah');
+            expect(controller.isTabActive('/dashboard') == 'active').toBe(true);
+            expect(controller.isTabActive('/reports') == 'active').toBe(false);
+            expect(controller.isTabActive('/preferences') == 'active').toBe(false);
         });
     });
 });
