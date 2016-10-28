350b35b50a21b99310671125b4394b45e6b53767
dkavanagh <dkavanagh@gmail.com>
2016-10-27 22:31:45 -0400

    GUI-2810 checkboxes and loading indicator work

--- eucaconsole/static/js/widgets/instance-selector2.js
+++ eucaconsole/static/js/widgets/instance-selector2.js
@@ -13,7 +13,7 @@ angular.module('InstancesSelectorModule', ['InstancesServiceModule', 'EucaConsol
         scope: {
         },
         templateUrl: '/_template/elbs/instance-selector',
-        controller: ['$scope', 'InstancesService', 'eucaHandleError', function ($scope, InstancesService, eucaHandleError) {
+        controller: ['$scope', '$timeout', 'InstancesService', 'eucaHandleError', function ($scope, $timeout, InstancesService, eucaHandleError) {
             InstancesService.getInstances($('#csrf_token').val()).then(
                 function success(result) {
                     $scope.instanceList = result;
@@ -23,7 +23,34 @@ angular.module('InstancesSelectorModule', ['InstancesServiceModule', 'EucaConsol
                     eucaHandleError(errData.data.message, errData.status);
                     $scope.instancesLoading = false;
                 });
+            $scope.instanceList = [];
             $scope.instancesLoading = true;
+            $scope.state = {'allSelected': false};
+            $scope.setAllState = function() {
+                $timeout(function() {
+                    angular.forEach($scope.instanceList, function(item) {
+                        item.selected = $scope.state.allSelected;
+                    });
+                });
+            };
+            $scope.handleItemSelection = function() {
+                // set all checkbox state based on state of items.selected
+                var allItemsCheckbox = document.getElementById('instance-all-checkbox');
+                var checkedIems = $scope.instanceList.filter(function (item) {
+                    return item.selected;
+                });
+                if (!checkedIems.length) {
+                    $scope.state.allSelected = false;
+                }
+                // Set indeterminate state on select-all checkbox when checked and at least one item is unselected
+                if (allItemsCheckbox) {
+                    allItemsCheckbox.indeterminate = !!($scope.state.allSelected && checkedIems.length < $scope.instanceList.length);
+                }
+                if (!$scope.state.allSelected && checkedIems.length === $scope.instanceList.length) {
+                    $scope.state.allSelected = true;
+                }
+                $scope.selectedItems = checkedIems;
+            };
         }]
-    }
+    };
 }]);
