ffdb6745f1f04fdd5f58492b0c773c903b7f6d0c
Kamal Gill <kamal@kamalgill.net>
2016-11-04 18:30:02 -0700

    GUI-2818: Initial progress on new ELB Wizard Network tab

--- eucaconsole/static/js/pages/elb-wizard/network.js
+++ eucaconsole/static/js/pages/elb-wizard/network.js
@@ -1,4 +1,29 @@
 angular.module('ELBWizard')
-.controller('NetworkController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    console.log('network');
+.controller('NetworkController', ['$scope', function ($scope) {
+    var vm = this;
+
+    vm.vpcNetwork = '';
+    vm.securityGroups = [];
+    vm.vpcNetworkChoices = [{id:'vpc-123456', label:'vpc-123456'}];
+    vm.securityGroupChoices = [{id:'default', label:'default'}];
+
+    // Set initial VPC network choice
+    if (vm.vpcNetworkChoices.length) {
+        vm.vpcNetwork = vm.vpcNetworkChoices[0];
+    }
+
+    // Set initial security groups choice to 'default' group
+    if (vm.securityGroupChoices.length) {
+        vm.securityGroups = vm.securityGroupChoices.filter(function (sgroup) {
+            return sgroup.id === 'default';
+        });
+    }
+
+    vm.submit = function () {
+        if (vm.networkForm.$invalid) {
+            return;
+        }
+
+        ELBWizardService.next({});
+    };
 }]);
