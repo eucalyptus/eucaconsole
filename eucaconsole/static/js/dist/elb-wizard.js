98be19731511302ae9ed1b44416c03db8f65f3cd
dkavanagh <dkavanagh@gmail.com>
2016-11-16 12:22:15 -0500

    GUI-2818 fixed vm.networkForm error

--- eucaconsole/static/js/dist/elb-wizard.js
+++ eucaconsole/static/js/dist/elb-wizard.js
@@ -468,14 +468,13 @@ angular.module('ELBWizard')
 angular.module('ELBWizard')
 .controller('NetworkController', ['$scope', 'ELBWizardService', function ($scope, ELBWizardService) {
     var vm = this;
-
     vm.vpcNetwork = ELBWizardService.values.vpcNetwork;
     vm.vpcNetworkChoices = ELBWizardService.values.vpcNetworkChoices;
     vm.vpcSecurityGroups = ELBWizardService.values.vpcSecurityGroups;
     vm.vpcSecurityGroupChoices = ELBWizardService.values.vpcSecurityGroupChoices;
 
     vm.submit = function () {
-        if (vm.networkForm.$invalid) {
+        if ($scope.networkForm.$invalid) {
             return;
         }
 
--- eucaconsole/static/js/pages/elb-wizard/network.js
+++ eucaconsole/static/js/pages/elb-wizard/network.js
@@ -1,14 +1,13 @@
 angular.module('ELBWizard')
 .controller('NetworkController', ['$scope', 'ELBWizardService', function ($scope, ELBWizardService) {
     var vm = this;
-
     vm.vpcNetwork = ELBWizardService.values.vpcNetwork;
     vm.vpcNetworkChoices = ELBWizardService.values.vpcNetworkChoices;
     vm.vpcSecurityGroups = ELBWizardService.values.vpcSecurityGroups;
     vm.vpcSecurityGroupChoices = ELBWizardService.values.vpcSecurityGroupChoices;
 
     vm.submit = function () {
-        if (vm.networkForm.$invalid) {
+        if ($scope.networkForm.$invalid) {
             return;
         }
 
