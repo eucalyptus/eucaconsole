79c6340a79bfc9b793eeb57397356f7a06df513a
Kamal Gill <kamal@kamalgill.net>
2017-01-26 12:34:53 -0800

    GUI-2829: Initial progress adding a route entry

--- eucaconsole/static/js/widgets/route-table-editor.js
+++ eucaconsole/static/js/widgets/route-table-editor.js
@@ -7,21 +7,55 @@ angular.module('RouteTableEditorModule', [])
             require: 'ngModel',
             templateUrl: '/_template/vpcs/route-table-editor',
             controller: function ($scope) {
+                $scope.destinationCidrBlock = '';
+                $scope.routeTarget = '';
+
+                $scope.$watch('destinationCidrBlock', requireOther('routeTarget'));
+                $scope.$watch('routeTarget', requireOther('destinationCidrBlock'));
+
+                function requireOther (other) {
+                    return function (newVal) {
+                        if(newVal === '' || $scope[other] === '') {
+                            $scope.routeTableForm.$setPristine();
+                        }
+                    };
+                }
+
                 $scope.addRoute = function ($event) {
                     $event.preventDefault();
 
+                    if ($scope.destinationCidrBlock === '' || $scope.routeTarget === '') {
+                        return;
+                    }
+
+                    if ($scope.routeTableForm.$invalid || $scope.routeTableForm.$pristine) {
+                        return;
+                    }
+
                     var route = {
-                        // TODO: Pull input values
+                        destination_cidr_block: $scope.destinationCidrBlock,
+                        state: 'active',
+                        // TODO: Set interface_id instead if ENI selected
+                        gateway_id: $scope.routeTarget
                     };
 
                     $scope.routes.push(route);
+                    resetForm();
                     $scope.$emit('routeTableUpdated');
+                };
+
+                var resetForm = function () {
+                    $scope.destinationCidrBlock = '';
+                    $scope.routeTarget = '';
+                    $scope.routeTableForm.$setPristine();
+                    $scope.routeTable.$setUntouched();
                 };
 
             },
             link: function (scope, element, attrs, ctrl, transcludeContents) {
                 var content = transcludeContents();
                 scope.routes = JSON.parse(content.text() || '[]');
+                scope.cidrPattern = attrs.cidrPattern;
 
                 scope.updateViewValue = function () {
                     ctrl.$setViewValue(scope.routes);
