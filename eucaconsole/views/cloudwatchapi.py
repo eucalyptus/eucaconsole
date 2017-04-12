d1bcf9d8c42ba34b9bd2b980cd18252fe9b3b4c8
Kamal Gill <kamal@kamalgill.net>
2017-04-12 11:13:00 -0700

    GUI-2896: Remove black from chart line colors

--- eucaconsole/views/cloudwatchapi.py
+++ eucaconsole/views/cloudwatchapi.py
@@ -45,12 +45,12 @@ from ..views import BaseView, boto_error_handler
 CHART_COLORS = {
     0: '#00c9ff',  # $dxc-blue
     1: '#64ff00',  # $dxc-green
-    2: '#000000',  # $dxc-black
-    3: '#ffed00',  # $dxc-yellow
-    4: 'purple',  # CSS Level 1 color
-    5: 'navy',  # CSS Level 1 color
-    6: 'olive',  # CSS Level 1 color
-    7: 'teal',  # CSS Level 1 color
+    2: '#ffed00',  # $dxc-yellow
+    3: 'purple',  # CSS Level 1 color
+    4: 'navy',  # CSS Level 1 color
+    5: 'teal',  # CSS Level 1 color
+    6: 'green',  # CSS Level 1 color
+    7: 'olive',  # CSS Level 1 color
 }
 
 ISO8601 = '%Y-%m-%dT%H:%M:%S.%fZ'
